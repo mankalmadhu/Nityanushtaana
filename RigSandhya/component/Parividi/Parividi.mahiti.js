c9f64995e6d9d152a6f2a8fec8a933d1b0446d41
Madhu <mankalmadhu@gmail.com>
2017-08-12 03:38:50 +0530

    Data  test

--- RigSandhya/component/Parividi/Parividi.mahiti.js
+++ RigSandhya/component/Parividi/Parividi.mahiti.js
@@ -1,35 +1,35 @@
 
-export const sandhyaChitra = require('./chitra/sandhya.png');
-export const agnikaryaChitra = require('./chitra/agnikarya.png');
-export const upadeshaChitra = require('./chitra/upadesha.png');
+export const sandhyaChitra = () => require('./chitra/sandhya.png');
+export const agnikaryaChitra = () => require('./chitra/agnikarya.png');
+export const upadeshaChitra = () => require('./chitra/upadesha.png');
 
-export const parividiMahiti = [
+export const parividiMahiti = () => [
   {
     "shershike":  "ಪ್ರಾತಸ್ಸ೦ಧ್ಯಾ",
-    "chitra": exports.sandhyaChitra
+    "chitra": exports.sandhyaChitra()
   },
   {
     "shershike":  "ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ",
-    "chitra": exports.agnikaryaChitra
+    "chitra": exports.agnikaryaChitra()
   },
   {
     "shershike":  "ಮಾಧ್ಯಾಹ್ನಿಕಸ೦ಧ್ಯಾ",
-    "chitra": exports.sandhyaChitra
+    "chitra": exports.sandhyaChitra()
   },
   {
     "shershike":  "ಸಾಯ೦ಸ೦ಧ್ಯಾ",
-    "chitra": exports.sandhyaChitra
+    "chitra": exports.sandhyaChitra()
   },
   {
     "shershike":  "ಸಾಯ೦ಸಮಿಧಾದಾನ",
-    "chitra": exports.agnikaryaChitra
+    "chitra": exports.agnikaryaChitra()
   },
   {
     "shershike":  "ಯಜ್ಞೋಪವೀತ",
-    "chitra": exports.upadeshaChitra
+    "chitra": exports.upadeshaChitra()
   },
   {
     "shershike":  "ಉಪನಯನಕಾರಿಕ",
-    "chitra": exports.upadeshaChitra
+    "chitra": exports.upadeshaChitra()
   }
   ]
---
+++ RigSandhya/component/Parividi/__tests__/Parividi.mahiti.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+jest.unmock('../Parividi.mahiti');
+const mahiti =require('../Parividi.mahiti');
+
+test("Parividi Mahiti" , () => {
+  mahiti.sandhyaChitra = jest.fn(()=>'./chitra/sandhya.png');
+  mahiti.agnikaryaChitra = jest.fn(()=>'./chitra/agnikarya.png');
+  mahiti.upadeshaChitra = jest.fn(()=>'./chitra/upadesha.png');
+  expect(<div>{mahiti.parividiMahiti()}</div>).toMatchSnapshot();
+});
