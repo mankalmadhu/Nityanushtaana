62b61f591a43dcf0e646cbf3dea1b701ff2b77f4
mahobalan <mankalmadhu@gmail.com>
2017-08-18 18:00:17 +0200

    Get Rid OF React navigation

--- RigSandhya/component/Shreekara/Shreekara.js
+++ RigSandhya/component/Shreekara/Shreekara.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import shreekaraVinyasa from './Shreekara.vinyasa';
-import { StackNavigator } from 'react-navigation';
 
 export default class Shreekara extends React.Component {
-// Nav options can be defined as a function of the screen's props:
-  static navigationOptions = ({ navigation }) => ({
-    title: `${navigation.state.params.shershike}`,
-  });
 
   render()
   {
