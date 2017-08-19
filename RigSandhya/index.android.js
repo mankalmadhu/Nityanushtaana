9cdc48eed773eafed373e6a4ee9c83b65e2b3757
Madhu AHOBALAN <mahobalan@amadeus.com>
2017-08-19 17:00:10 +0200

    Fix Index android tests

--- RigSandhya/index.android.js
+++ RigSandhya/index.android.js
@@ -18,19 +18,19 @@ import Parividi from './component/Parividi/Parividi';
 import Shreekara from './component/Shreekara/Shreekara';
 import Shershike from './component/Shershike/Shershike';
  
-const RigSandhya = () => (
+export default RigSandhya = () => (
   
   <NativeRouter style={styles.container} >
     <Navigation navBarStyle={{backgroundColor: '#C1C1C1'}}>
-      <Card exact path="/" component={Parividi} renderTitle = {()=>(<ParividiShershike shershike='ಋಗ್ವೇದ ಸ೦ಧ್ಯಾವ೦ದನ'/>)}/>
-      <Card path="/ಪ್ರಾತಸ್ಸ೦ಧ್ಯಾ"  component={Shreekara} renderTitle = {()=>(<ParividiShershike shershike='ಪ್ರಾತಸ್ಸ೦ಧ್ಯಾ'/>)} />
-      <Card path="/ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ"  component={Shreekara} renderTitle = {()=>(<ParividiShershike shershike='ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ'/>)} />
-      <Card path="/ಮಾಧ್ಯಾಹ್ನಿಕಸ೦ಧ್ಯಾ"  component={Shreekara} renderTitle = {()=>(<ParividiShershike shershike='ಮಾಧ್ಯಾಹ್ನಿಕಸ೦ಧ್ಯಾ'/>)} />
-      <Card path="/ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ"  component={Shreekara} renderTitle = {()=>(<ParividiShershike shershike='ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ'/>)} />
-      <Card path="/ಸಾಯ೦ಸ೦ಧ್ಯಾ"  component={Shreekara} renderTitle = {()=>(<ParividiShershike shershike='ಸಾಯ೦ಸ೦ಧ್ಯಾ'/>)} />
-      <Card path="/ಸಾಯ೦ಸಮಿಧಾದಾನ"  component={Shreekara} renderTitle = {()=>(<ParividiShershike shershike='ಸಾಯ೦ಸಮಿಧಾದಾನ'/>)} />
-      <Card path="/ಯಜ್ಞೋಪವೀತ"  component={Shreekara} renderTitle = {()=>(<ParividiShershike shershike='ಯಜ್ಞೋಪವೀತ'/>)} />
-      <Card path="/ಉಪನಯನಕಾರಿಕ"  component={Shreekara} renderTitle = {()=>(<ParividiShershike shershike='ಉಪನಯನಕಾರಿಕ'/>)} />
+      <Card exact path="/" component={Parividi} renderTitle = {()=>(<Shershike shershike='ಋಗ್ವೇದ ಸ೦ಧ್ಯಾವ೦ದನ'/>)}/>
+      <Card path="/ಪ್ರಾತಸ್ಸ೦ಧ್ಯಾ"  component={Shreekara} renderTitle = {()=>(<Shershike shershike='ಪ್ರಾತಸ್ಸ೦ಧ್ಯಾ'/>)} />
+      <Card path="/ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ"  component={Shreekara} renderTitle = {()=>(<Shershike shershike='ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ'/>)} />
+      <Card path="/ಮಾಧ್ಯಾಹ್ನಿಕಸ೦ಧ್ಯಾ"  component={Shreekara} renderTitle = {()=>(<Shershike shershike='ಮಾಧ್ಯಾಹ್ನಿಕಸ೦ಧ್ಯಾ'/>)} />
+      <Card path="/ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ"  component={Shreekara} renderTitle = {()=>(<Shershike shershike='ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ'/>)} />
+      <Card path="/ಸಾಯ೦ಸ೦ಧ್ಯಾ"  component={Shreekara} renderTitle = {()=>(<Shershike shershike='ಸಾಯ೦ಸ೦ಧ್ಯಾ'/>)} />
+      <Card path="/ಸಾಯ೦ಸಮಿಧಾದಾನ"  component={Shreekara} renderTitle = {()=>(<Shershike shershike='ಸಾಯ೦ಸಮಿಧಾದಾನ'/>)} />
+      <Card path="/ಯಜ್ಞೋಪವೀತ"  component={Shreekara} renderTitle = {()=>(<Shershike shershike='ಯಜ್ಞೋಪವೀತ'/>)} />
+      <Card path="/ಉಪನಯನಕಾರಿಕ"  component={Shreekara} renderTitle = {()=>(<Shershike shershike='ಉಪನಯನಕಾರಿಕ'/>)} />
     </Navigation>
    </NativeRouter>
 )
