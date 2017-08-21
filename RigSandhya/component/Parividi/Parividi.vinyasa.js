98f8b877fbff491e7dbdb4145167f1ce4f64e94c
mahobalan <mankalmadhu@gmail.com>
2017-08-21 18:54:50 +0200

    Get Rid of annoying margin

--- RigSandhya/component/Parividi/Parividi.vinyasa.js
+++ RigSandhya/component/Parividi/Parividi.vinyasa.js
@@ -3,7 +3,6 @@ import {StyleSheet} from 'react-native';
 export default parividiVinyasa = StyleSheet.create({
   avarana: {
     flex: 3,
-    marginTop: 2,
     backgroundColor: '#333333'
   },
   vibhajaka: {
