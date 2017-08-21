21db505417fc8d97eaca0c2a0730e3110fa7040c
mahobalan <mankalmadhu@gmail.com>
2017-08-21 18:47:07 +0200

    More content in shrreekars

--- RigSandhya/component/Shreekara/Shreekara.js
+++ RigSandhya/component/Shreekara/Shreekara.js
@@ -11,6 +11,10 @@ export default class Shreekara extends React.Component {
     <Text style={shreekaraVinyasa.pathya}>
       ಓ೦ ಶ್ರೀ ಗಣಪತಿ ಶಾರದ ಗುರುಭ್ಯೋ ನಮಃ ॥
     </Text>
+    <Text style={shreekaraVinyasa.pathya}>
+      ಗ॒ಣಾನಾ᳚೦ ತ್ವಾ ಗ॒ಣಪ॑ತಿಗ್೦ ಹವಾಮಹೇ ಕ॒ವಿ೦  ಕ॑ವೀ॒ನಾಮು॑ಪ॒ಮಶ್ರ॑ವಸ್ತಮ೦ । ಜ್ಯೇ॒ಷ್ಥ॒ರಾಜ॒೦ ಬ್ರಹ್ಮ॑ಣಾ೦ ಬ್ರಹ್ಮಣಸ್ಪತ॒ ಆ ನಃ॑ ಶೃ॑ಣ್ವನ್ನೂ॒ತಿಭಿ॑ಸ್ಸೀದ॒ ಸಾದ॑ನ೦॥ ಓ೦ ಶಾ೦ತಿಃ ಶಾ೦ತಿಃ ಶಾ೦ತಿಃ ।
+    </Text>
+
   </View>
     );
   }
