3e90e3fceeec82773196bce6438c218995f02de1
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-06 23:41:11 +0200

    update diksoochi ut

--- RigSandhya/component/Diksoochi/__tests__/Diksoochi.js
+++ RigSandhya/component/Diksoochi/__tests__/Diksoochi.js
@@ -18,7 +18,7 @@ jest.mock('react-router-navigation', (props)=>{
       var props = {
         path:this.props.path,
         component:this.props.component.name,
-        renderTitle:this.props.renderTitle().props.shershike};
+        renderTitle:this.props.renderTitle()};
         
       return MockReact.createElement('Card' ,props);
     }
