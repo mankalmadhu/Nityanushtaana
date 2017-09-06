fa89b6fa9a98b05166ce0ca0ea7636a76f2d037b
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-06 22:56:12 +0200

    Scroll scrolls

--- RigSandhya/component/PratahSandhya/PratahSandhya.vinyasa.js
+++ RigSandhya/component/PratahSandhya/PratahSandhya.vinyasa.js
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native';
 
 export default pratahSandhyaVinyasa = StyleSheet.create({
   avarana: {
-    flex: 1,
     padding: 3,
     flexDirection: 'column',
     alignItems: 'center',
