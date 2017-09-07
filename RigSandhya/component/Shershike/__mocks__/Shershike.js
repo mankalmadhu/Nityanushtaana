8a907554bef92858787d3fbfce893702d045ab7e
Madhu AHOBALAN <mahobalan@amadeus.com>
2017-09-07 18:48:03 +0200

    Revert "Shershike updated"
    This reverts commit 7ffff28db99ad2d02a3699506f678a72e4b553a7.

---
+++ RigSandhya/component/Shershike/__mocks__/Shershike.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default Shershike = (props) => (
+  <Text>
+    {props.shershike}
+  </Text>
+);
--- RigSandhya/component/Shershike/__tests__/Shershike.js
+++ RigSandhya/component/Shershike/__tests__/Shershike.js
@@ -1,14 +1,13 @@
 import 'react-native';
 import React from 'react';
 import Shershike from '../Shershike';
-import { StyleSheet } from 'react-native';
 
 import Renderer from 'react-test-renderer';
 
 test("Shershike Render" , () => {
 
    const shershike = Renderer.create(
-       <Shershike shershike='TestTitle' vinyasa={StyleSheet.create({avarana:{},pathya:{}})}/>
+       <Shershike shershike='TestTitle'/>
    ).toJSON();
 
    expect(shershike).toMatchSnapshot();
