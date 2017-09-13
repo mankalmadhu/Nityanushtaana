84cb47598a2251a7fbba6258d3943f0a479785a0
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-13 22:05:01 +0200

    Mock Dwiteya Marjane

--- RigSandhya/component/Ghatta/DwiteyaMarjane/__tests__/DwiteyaMarjane.js
+++ RigSandhya/component/Ghatta/DwiteyaMarjane/__tests__/DwiteyaMarjane.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ದ್ವಿತೀಯಮಾರ್ಜನೆ from '../DwiteyaMarjane';
 import Renderer from 'react-test-renderer';
 
+jest.mock('../../Marjane/Marjane',()=>'ಮಾರ್ಜನೆ');
+jest.mock('../../Gayatri/GayatriMantra',()=>'ಗಾಯತ್ರೀಮ೦ತ್ರ');
+jest.mock('../../Gayatri/Vyahruti',()=>'ವ್ಯಾಹೃತಿ');
+
 test("ದ್ವಿತೀಯಮಾರ್ಜನೆ", () => {
   const marjane = Renderer.create(
     <ದ್ವಿತೀಯಮಾರ್ಜನೆ />
