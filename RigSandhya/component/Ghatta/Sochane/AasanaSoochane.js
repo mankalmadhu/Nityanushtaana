50eee225eb161acce9dd5fdb311b8645e1b2eb98
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-14 00:12:09 +0200

    Use component vishaya

--- RigSandhya/component/Ghatta/Sochane/AasanaSoochane.js
+++ RigSandhya/component/Ghatta/Sochane/AasanaSoochane.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import ghattaVinyasa from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
 
 export default ಆಸನಸೂಚನೆ = () => (
-    <Text style={ghattaVinyasa.ghattaShershike}>
-        (ಪೂರ್ವ/ಉತ್ತರ ಅಭಿಮುಖವಾಗಿ ಕುಳಿತು)
-    </Text>
-);
+    <ವಿಷಯ 
+    ವಿನ್ಯಾಸ = {ghattaVinyasa.ghattaShershike} 
+    ಮಾಹಿತಿ='(ಪೂರ್ವ/ಉತ್ತರ ಅಭಿಮುಖವಾಗಿ ಕುಳಿತು)'/>
+ );
+ 
--- RigSandhya/component/Ghatta/Sochane/__tests__/AasanaSoochane.js
+++ RigSandhya/component/Ghatta/Sochane/__tests__/AasanaSoochane.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ಆಸನಸೂಚನೆ from '../AasanaSoochane';
 import Renderer from 'react-test-renderer';
+import ವಿಷಯ from '../../../Vishaya/Vishaya';
+
+jest.mock('../../Ghatta.vinyasa',()=>{return {ghattaShershike:'ghattaShershike'}});
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
 
 test("ಆಸನಸೂಚನೆ" , () => {
   const aasanaSochane = Renderer.create(
