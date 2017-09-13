e552f1ae992368afa4dcd94949a859de560ad98a
Madhu AHOBALAN <mahobalan@amadeus.com>
2017-09-13 18:39:52 +0200

    Decompose Sankalpa

--- RigSandhya/component/Ghatta/Sankalpa/PratahSandhyaSankalpa.js
+++ RigSandhya/component/Ghatta/Sankalpa/PratahSandhyaSankalpa.js
@@ -4,20 +4,17 @@ import { View, Text } from 'react-native';
 import ghattaVinyasa from '../Ghatta.vinyasa';
 import ಗಾಯತ್ರೀಮ೦ತ್ರ from '../Gayatri/GayatriMantra';
 import ವ್ಯಾಹೃತಿ from '../Gayatri/Vyahruti';
+import ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ from './SankalpaPurvanga';
+import ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ from './SankalpaShershike';
+import ಓಮಾಪೋ from './Oomapo';
 
 export default  ಪ್ರಾತಃಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ = () => (
   <View>
-    <Text style={ghattaVinyasa.ghattaShershike}>
-       ಸ೦ಕಲ್ಪ
-    </Text>
-    <Text style={ghattaVinyasa.purvanga}>
-       ಪ್ರಣವಸ್ಯ ಪರಬ್ರಹ್ಮ ಋಷಿಃ ಪರಮಾತ್ಮಾ ದೇವತಾ ದೈವೀ ಗಾಯತ್ರೀ ಛ೦ದಃ ಪ್ರಾಣಾಯಾಮೇ ವಿನಿಯೋಗಃ ।
-    </Text>
+     <ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ />
+     <ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ/>
      <ವ್ಯಾಹೃತಿ />
      <ಗಾಯತ್ರೀಮ೦ತ್ರ />
-     <Text style={ghattaVinyasa.viniyoga}>
-       ಓ॒ಮಾಪೋ॒ ಜ್ಯೋತೀ॒ರಸೋ॒ಽಮೃತ॒೦ ಬ್ರಹ್ಮ॒ ಭೂರ್ಭುವ॒ಸ್ಸ್ವರೋ೦॥
-    </Text>
+     <ಓಮಾಪೋ />
     <Text style={ghattaVinyasa.viniyoga}>
        ಮಮ ಉಪಾತ್ತ ಸಮಸ್ತ ದುರಿತಕ್ಷಯದ್ವಾರಾ ಶ್ರೀ ಪರಮೇಶ್ವರ ಪ್ರೀತ್ಯರ್ಥ೦ ಪ್ರಾತಃ ಸ೦ಧ್ಯಾ೦ ಉಪಾಸ್ಯೇ ॥
     </Text>
--- RigSandhya/component/Ghatta/Sankalpa/__tests__/PratahSandhyaSankalpa.js
+++ RigSandhya/component/Ghatta/Sankalpa/__tests__/PratahSandhyaSankalpa.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ಪ್ರಾತಃಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ from '../PratahSandhyaSankalpa';
 import Renderer from 'react-test-renderer';
 
+jest.mock('../../Gayatri/GayatriMantra', () => 'ಗಾಯತ್ರೀಮ೦ತ್ರ' );
+jest.mock('../../Gayatri/Vyahruti', () => 'ವ್ಯಾಹೃತಿ' );
+jest.mock('../SankalpaPurvanga', () => 'ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ' );
+jest.mock('../SankalpaShershike', () => 'ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ' );
+jest.mock('../Oomapo', () => 'ಓಮಾಪೋ' );
+
 test("ಪ್ರಾತಃಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ", () => {
   const pratahSandhyaSankalpa = Renderer.create(
     <ಪ್ರಾತಃಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ />
