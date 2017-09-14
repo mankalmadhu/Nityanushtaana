ab670335490fddbc42c3d111fadf5f925d307704
Madhu AHOBALAN <mahobalan@amadeus.com>
2017-09-14 14:19:24 +0200

    Visarjane

--- RigSandhya/component/Ghatta/DwiteyaMarjane/__tests__/Visarjane.js
+++ RigSandhya/component/Ghatta/DwiteyaMarjane/__tests__/Visarjane.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ವಿಸರ್ಜನೆ from '../Visarjane';
 import Renderer from 'react-test-renderer';
 
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+
 test("ವಿಸರ್ಜನೆ", () => {
   const pratahAbhimantrane = Renderer.create(
     <ವಿಸರ್ಜನೆ />
