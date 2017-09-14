ec4f8146c3c7bafd97316d214f3a9a4e49189fc3
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-14 23:26:44 +0200

    Pratah Abhimantrane

--- RigSandhya/component/Ghatta/Marjane/__tests__/PratahAbhimantrane.js
+++ RigSandhya/component/Ghatta/Marjane/__tests__/PratahAbhimantrane.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ಪ್ರಾತಃಅಭಿಮ೦ತ್ರಣೆ from '../PratahAbhimantrane';
 import Renderer from 'react-test-renderer';
 
-test("ಪ್ರಾತಃಅಭಿಮ೦ತ್ರಣೆ", () => {
-  const pratahAbhimantrane = Renderer.create(
-    <ಪ್ರಾತಃಅಭಿಮ೦ತ್ರಣೆ />
-  ).toJSON();
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
 
-  expect(pratahAbhimantrane).toMatchSnapshot();
+test("ಪ್ರಾತಃಅಭಿಮ೦ತ್ರಣೆ", () => {
+  expect(Renderer.create(
+    <ಪ್ರಾತಃಅಭಿಮ೦ತ್ರಣೆ />
+  ).toJSON()).toMatchSnapshot();
 
 });
