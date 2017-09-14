58a0e552f8d100bb9a8934b8633cd602ab118427
Madhu AHOBALAN <mahobalan@amadeus.com>
2017-09-14 12:43:21 +0200

    Dwiteya Marjane

--- RigSandhya/component/Ghatta/DwiteyaMarjane/__tests__/DwiteyaMarjane.js
+++ RigSandhya/component/Ghatta/DwiteyaMarjane/__tests__/DwiteyaMarjane.js
@@ -5,6 +5,8 @@ import Renderer from 'react-test-renderer';
 jest.mock('../../Marjane/Marjane',()=>'ಮಾರ್ಜನೆ');
 jest.mock('../../Gayatri/GayatriMantra',()=>'ಗಾಯತ್ರೀಮ೦ತ್ರ');
 jest.mock('../../Gayatri/Vyahruti',()=>'ವ್ಯಾಹೃತಿ');
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
 
 test("ದ್ವಿತೀಯಮಾರ್ಜನೆ", () => {
   const marjane = Renderer.create(
