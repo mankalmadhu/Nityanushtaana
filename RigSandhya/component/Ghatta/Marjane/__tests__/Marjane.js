213b202789e6d94a90262605acf4a235d2fe3406
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-14 23:15:48 +0200

    Marjane

--- RigSandhya/component/Ghatta/Marjane/__tests__/Marjane.js
+++ RigSandhya/component/Ghatta/Marjane/__tests__/Marjane.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ಮಾರ್ಜನೆ from '../Marjane';
 import Renderer from 'react-test-renderer';
 
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+
 test("ಮಾರ್ಜನೆ", () => {
   const marjane = Renderer.create(
     <ಮಾರ್ಜನೆ />
