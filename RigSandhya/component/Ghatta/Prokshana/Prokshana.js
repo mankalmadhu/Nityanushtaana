4a0e045521896ea6f888d9fe52f3d31bf88b3936
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-14 23:33:56 +0200

    Prokshana Migrated

--- RigSandhya/component/Ghatta/Prokshana/Prokshana.js
+++ RigSandhya/component/Ghatta/Prokshana/Prokshana.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import ghattaVinyasa from '../Ghatta.vinyasa';
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
+
 
 export default ಪ್ರೋಕ್ಷಣ = () => (
   <View>
-    <Text style={ghattaVinyasa.ghattaShershike}>
-        ಪ್ರೋಕ್ಷಣ
-    </Text>
-    <Text style={ghattaVinyasa.viniyoga}>
-       ಅಪವಿತ್ರಃ ಪವಿತ್ರೋವಾ ಸರ್ವಾವಸ್ಥಾ೦ ಗತೋಽಪಿವಾ । ಯಸ್ಸ್ಮರೇತ್ ಪು೦ಡರೀಕಾಕ್ಷ೦ ಸಬಾಹ್ಯಾಭ್ಯ೦ತರಶ್ಶುಚಿಃ ।
-    </Text>
+    <ವಿಷಯ 
+    ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ಶೀರ್ಷಿಕೆ} 
+    ಮಾಹಿತಿ={`ಪ್ರೋಕ್ಷಣ`}/>    
+
+    <ವಿಷಯ 
+    ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ವಿನಿಯೋಗ} 
+    ಮಾಹಿತಿ={`ಅಪವಿತ್ರಃ ಪವಿತ್ರೋವಾ ಸರ್ವಾವಸ್ಥಾ೦ ಗತೋಽಪಿವಾ ।
+     ಯಸ್ಸ್ಮರೇತ್ ಪು೦ಡರೀಕಾಕ್ಷ೦ ಸಬಾಹ್ಯಾಭ್ಯ೦ತರಶ್ಶುಚಿಃ ।`}/>    
+
   </View>
 
 );
--- RigSandhya/component/Ghatta/Prokshana/__tests__/Prokshana.js
+++ RigSandhya/component/Ghatta/Prokshana/__tests__/Prokshana.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ಪ್ರೋಕ್ಷಣ from '../Prokshana';
 import Renderer from 'react-test-renderer';
 
-test("ಪ್ರೋಕ್ಷಣ", () => {
-  const prokshana = Renderer.create(
-    <ಪ್ರೋಕ್ಷಣ />
-  ).toJSON();
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
 
-  expect(prokshana).toMatchSnapshot();
+test("ಪ್ರೋಕ್ಷಣ", () => {
+  expect(Renderer.create(
+    <ಪ್ರೋಕ್ಷಣ />
+  ).toJSON()).toMatchSnapshot();
 
 });
