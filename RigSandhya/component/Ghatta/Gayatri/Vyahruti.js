6453ca5acfd5b648347c9533d706d03f63852243
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-14 22:25:52 +0200

    Vyahruti migrated

--- RigSandhya/component/Ghatta/Gayatri/Vyahruti.js
+++ RigSandhya/component/Ghatta/Gayatri/Vyahruti.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import ghattaVinyasa from '../Ghatta.vinyasa';
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
 
 export default ವ್ಯಾಹೃತಿ = () => (
-   <Text style={ghattaVinyasa.viniyoga}>
-      ಓ೦ ಭೂಃ ಓ೦ ಭುವಃ ಓ೦ ಸುವಃ ಓ೦ ಮಹಃ ಓ೦ ಜನಃ ಓ೦ ತಪಃ ಓ೦ ಸತ್ಯಮ್ ॥
-   </Text>
+<ವಿಷಯ 
+ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ವಿನಿಯೋಗ} 
+ಮಾಹಿತಿ={`ಓ೦ ಭೂಃ ಓ೦ ಭುವಃ ಓ೦ ಸುವಃ ಓ೦ ಮಹಃ ಓ೦ ಜನಃ ಓ೦ ತಪಃ ಓ೦ ಸತ್ಯಮ್ ॥`}/>    
+
     
 );
--- RigSandhya/component/Ghatta/Gayatri/__tests__/Vyahruti.js
+++ RigSandhya/component/Ghatta/Gayatri/__tests__/Vyahruti.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ವ್ಯಾಹೃತಿ from '../Vyahruti';
 import Renderer from 'react-test-renderer';
 
-test("ವ್ಯಾಹೃತಿ", () => {
-  const vyaaHruti = Renderer.create(
-    <ವ್ಯಾಹೃತಿ />
-  ).toJSON();
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
 
-  expect(vyaaHruti).toMatchSnapshot();
+test("ವ್ಯಾಹೃತಿ", () => {
+   expect(Renderer.create(
+    <ವ್ಯಾಹೃತಿ />
+  ).toJSON()).toMatchSnapshot();
 
 });
