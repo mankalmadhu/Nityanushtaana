dc1b5e4dddf0426f9e34a16444cec1be2f20e13a
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-16 13:07:22 +0200

    Preetyartha

---
+++ RigSandhya/component/Ghatta/Sankalpa/Preetyartha.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
+
+export default ಪ್ರೀತ್ಯರ್ಥ = () => (
+    <ವಿಷಯ 
+    ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ವಿನಿಯೋಗ} 
+    ಮಾಹಿತಿ={`ಮಮ ಉಪಾತ್ತ ಸಮಸ್ತ ದುರಿತಕ್ಷಯದ್ವಾರಾ ಶ್ರೀ ಪರಮೇಶ್ವರ ಪ್ರೀತ್ಯರ್ಥ೦`}/>    
+
+);
---
+++ RigSandhya/component/Ghatta/Sankalpa/__tests__/Preetyartha.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ಪ್ರೀತ್ಯರ್ಥ from '../Preetyartha';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+
+test("ಓಮಾಪೋ", () => {
+  expect(Renderer.create(
+    <ಪ್ರೀತ್ಯರ್ಥ />
+  ).toJSON()).toMatchSnapshot();
+
+});
