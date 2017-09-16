e537545bcf1e5b7ccdf397b79f6b65ede3f4f4a6
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-16 23:31:36 +0200

    Sankalpa Refactor

--- RigSandhya/component/Ghatta/Sankalpa/Preetyartha.js
+++ RigSandhya/component/Ghatta/Sankalpa/Preetyartha.js
@@ -4,9 +4,9 @@ import { View, Text } from 'react-native';
 import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
 import ವಿಷಯ from '../../Vishaya/Vishaya';
 
-export default ಪ್ರೀತ್ಯರ್ಥ = () => (
-    <ವಿಷಯ ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ವಿನಿಯೋಗ}>
-     ಮಮ ಉಪಾತ್ತ ಸಮಸ್ತ ದುರಿತಕ್ಷಯದ್ವಾರಾ ಶ್ರೀ ಪರಮೇಶ್ವರ ಪ್ರೀತ್ಯರ್ಥ೦,
+export default ಪ್ರೀತ್ಯರ್ಥ = (props) => (
+    <ವಿಷಯ ವಿನ್ಯಾಸ={ಘಟ್ಟವಿನ್ಯಾಸ.ವಿನಿಯೋಗ}>
+        ಮಮ ಉಪಾತ್ತ ಸಮಸ್ತ ದುರಿತಕ್ಷಯದ್ವಾರಾ ಶ್ರೀ ಪರಮೇಶ್ವರ ಪ್ರೀತ್ಯರ್ಥ೦ {props.children}
     </ವಿಷಯ>
 
 );
--- RigSandhya/component/Ghatta/Sankalpa/__tests__/Preetyartha.js
+++ RigSandhya/component/Ghatta/Sankalpa/__tests__/Preetyartha.js
@@ -3,11 +3,13 @@ import ಪ್ರೀತ್ಯರ್ಥ from '../Preetyartha';
 import Renderer from 'react-test-renderer';
 
 jest.mock('../../Ghatta.vinyasa');
-jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+jest.mock('../../../Vishaya/Vishaya', () => 'ವಿಷಯ');
 
 test("ಓಮಾಪೋ", () => {
   expect(Renderer.create(
-    <ಪ್ರೀತ್ಯರ್ಥ />
+    <ಪ್ರೀತ್ಯರ್ಥ>
+      ಉಪಾಸ್ಯೇ
+    </ಪ್ರೀತ್ಯರ್ಥ>
   ).toJSON()).toMatchSnapshot();
 
 });
