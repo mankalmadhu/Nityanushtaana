f13e58c7968df1b57d8c7e0debd68d505a4f223c
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-17 00:19:02 +0200

    Argya Sankalpa

---
+++ RigSandhya/component/Ghatta/Sankalpa/PratahArghyaSankalpa.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import ಗಾಯತ್ರೀಮ೦ತ್ರ from '../Gayatri/GayatriMantra';
+import ವ್ಯಾಹೃತಿ from '../Gayatri/Vyahruti';
+import ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ from './SankalpaPurvanga';
+import ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ from './SankalpaShershike';
+import ಓಮಾಪೋ from './Oomapo';
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
+import ಪ್ರೀತ್ಯರ್ಥ from './Preetyartha';
+
+export default ಪ್ರಾತರ್ರಘ್ಯಸ೦ಕಲ್ಪ = () => (
+  <View>
+    <ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ />
+    <ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ />
+    <ವ್ಯಾಹೃತಿ />
+    <ಗಾಯತ್ರೀಮ೦ತ್ರ />
+    <ಓಮಾಪೋ />
+    <ಪ್ರೀತ್ಯರ್ಥ>
+      <ವಿಷಯ ವಿನ್ಯಾಸ={ಘಟ್ಟವಿನ್ಯಾಸ.ವಿನಿಯೋಗ}>
+        ಪ್ರಾತಃ ಸ೦ಧ್ಯಾ೦ಗ ಅರ್ಘ್ಯ ಪ್ರದಾನಾನಿ ಕರಿಷ್ಯೇ
+     </ವಿಷಯ>
+    </ಪ್ರೀತ್ಯರ್ಥ>
+  </View>
+
+);
---
+++ RigSandhya/component/Ghatta/Sankalpa/__tests__/PratahArghyaSankalpa.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ಪ್ರಾತರ್ರಘ್ಯಸ೦ಕಲ್ಪ from '../PratahArghyaSankalpa';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Gayatri/GayatriMantra', () => 'ಗಾಯತ್ರೀಮ೦ತ್ರ' );
+jest.mock('../../Gayatri/Vyahruti', () => 'ವ್ಯಾಹೃತಿ' );
+jest.mock('../SankalpaPurvanga', () => 'ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ' );
+jest.mock('../SankalpaShershike', () => 'ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ' );
+jest.mock('../Preetyartha', () => 'ಪ್ರೀತ್ಯರ್ಥ' );
+jest.mock('../Oomapo', () => 'ಓಮಾಪೋ' );
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+
+test("ಪ್ರಾತಃಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ", () => {
+
+  expect(Renderer.create(
+    <ಪ್ರಾತರ್ರಘ್ಯಸ೦ಕಲ್ಪ />
+  ).toJSON()).toMatchSnapshot();
+
+});
