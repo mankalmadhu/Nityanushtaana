f7b74c5e18932dd81b9b4fd35fd8d25c151b4eab
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-20 22:23:52 +0200

    Kaalateeta Arghya

--- RigSandhya/component/Ghatta/ArghyaPradana/__tests__/PratahArghyaPradana.js
+++ RigSandhya/component/Ghatta/ArghyaPradana/__tests__/PratahArghyaPradana.js
@@ -7,6 +7,7 @@ jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
 jest.mock('../../Gayatri/Trihruti',()=>'ತ್ರಿಹೃತಿ');
 jest.mock('../../Gayatri/GayatriMantra',()=>'ಗಾಯತ್ರೀಮ೦ತ್ರ');
 jest.mock('../../Sankalpa/Oomapo',()=>'ಓಮಾಪೋ');
+jest.mock('../KalaTeetaArgyaPradana',()=>'ಕಾಲಾತೀತಅರ್ಘ್ಯ');
 
 test("ಪ್ರಾತರರ್ಘ್ಯ", () => {
    expect(Renderer.create(
