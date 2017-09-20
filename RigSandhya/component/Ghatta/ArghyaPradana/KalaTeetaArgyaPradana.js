698c94c9cf767a3bdb82e8f9b37f83f6eac3a31a
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-20 22:21:10 +0200

    Kalateeta Arghya

---
+++ RigSandhya/component/Ghatta/ArghyaPradana/KalaTeetaArgyaPradana.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
+import ತ್ರಿಹೃತಿ from '../Gayatri/Trihruti';
+import ಗಾಯತ್ರೀಮ೦ತ್ರ from '../Gayatri/GayatriMantra';
+import ಓಮಾಪೋ from '../Sankalpa/Oomapo';
+
+export default  ಕಾಲಾತೀತಅರ್ಘ್ಯ = () => (
+  <View>
+    <ವಿಷಯ ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ಪೂರ್ವಾ೦ಗ} >
+      ಕಾಲಾತೀತ ದೋಷ ಪ್ರಾಯಶ್ಚಿತ್ತಾರ್ಥ೦ ಅರ್ಘ್ಯ ಪ್ರದಾನ೦ ಕರಿಷ್ಯೇ ।
+    </ವಿಷಯ>
+    <ತ್ರಿಹೃತಿ>
+      <ಗಾಯತ್ರೀಮ೦ತ್ರ/>
+      <ಓಮಾಪೋ/>
+    </ತ್ರಿಹೃತಿ>
+
+  </View>
+
+);
---
+++ RigSandhya/component/Ghatta/ArghyaPradana/__tests__/KalaTeetaArgyaPradana.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ಕಾಲಾತೀತಅರ್ಘ್ಯ from '../KalaTeetaArgyaPradana';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+jest.mock('../../Gayatri/Trihruti',()=>'ತ್ರಿಹೃತಿ');
+jest.mock('../../Gayatri/GayatriMantra',()=>'ಗಾಯತ್ರೀಮ೦ತ್ರ');
+jest.mock('../../Sankalpa/Oomapo',()=>'ಓಮಾಪೋ');
+
+test("ಕಾಲಾತೀತಅರ್ಘ್ಯ", () => {
+   expect(Renderer.create(
+    <ಕಾಲಾತೀತಅರ್ಘ್ಯ />
+  ).toJSON()).toMatchSnapshot();
+ 
+});
