3bb76aa75ee1c138fb143f6c95f8205bc734adb8
Madhu Mankal <mankalmadhu@gmail.com>
2017-09-21 00:07:29 +0200

    Vishaya Vinyasa not mandatory

--- RigSandhya/component/Vishaya/Vishaya.js
+++ RigSandhya/component/Vishaya/Vishaya.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {  Text } from 'react-native';
-import PropTypes from 'prop-types';
 
 export default ವಿಷಯ = (props) => (
     <Text style={props.ವಿನ್ಯಾಸ}>
@@ -8,7 +7,3 @@ export default ವಿಷಯ = (props) => (
     </Text>
   
 );
-
-ವಿಷಯ.propTypes = {
-  ವಿನ್ಯಾಸ : PropTypes.object.isRequired
-};
