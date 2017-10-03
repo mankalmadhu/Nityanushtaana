706ce99a2d38cd99ee18e1c1c25fd943424feb5c
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-03 11:59:27 +0200

    Gayatri Japa

---
+++ RigSandhya/component/Ghatta/Japa/PratahGayatriJapa.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
+import ತ್ರಿಹೃತಿ from '../Gayatri/Trihruti';
+import ಪ್ರಾತಃಜಪಸ೦ಕಲ್ಪ from '../Sankalpa/PratahJapaSankalpa';
+import ಆವಾಹನ from './Aavahana';
+import ಗಾಯತ್ರೀಮ೦ತ್ರ from '../Gayatri/GayatriMantra';
+import ಕರನ್ಯಾಸ from './Karanyaasa';
+import ಅ೦ಗನ್ಯಾಸ from './Anganyaasa';
+import ದಿಗ್ಬ೦ಧನ from './Digbhanda';
+import ಪ್ರಾತಃಸ್ವರೂಪ from './PratahSwaroopa';
+import ಮ೦ತ್ರಾರ್ಥ from './Mantrartha';
+import ದಿಗ್ವಿಮೋಕ from './Digvimoka';
+import ಮುಕ್ತಾಯ from './Muktaya';
+
+
+export default ಪ್ರಾತಃಗಾಯತ್ರೀ = () => (
+  <ವಿಷಯ>
+    <ತ್ರಿಹೃತಿ />
+    <ತ್ರಿಹೃತಿ />
+    <ಆವಾಹನ />
+    <ಪ್ರಾತಃಜಪಸ೦ಕಲ್ಪ />
+    <ಕರನ್ಯಾಸ />
+    <ಅ೦ಗನ್ಯಾಸ />
+    <ದಿಗ್ಬ೦ಧನ />
+    <ಪ್ರಾತಃಸ್ವರೂಪ />
+    <ಮ೦ತ್ರಾರ್ಥ />
+    <ತ್ರಿಹೃತಿ>
+      <ಗಾಯತ್ರೀಮ೦ತ್ರ />
+    </ತ್ರಿಹೃತಿ>
+    <ವಿಷಯ>
+      ತತ್ಸಬ್ರಹ್ಮಾರ್ಪಣಮಸ್ತು
+    </ವಿಷಯ>
+    <ಕರನ್ಯಾಸ />
+    <ಅ೦ಗನ್ಯಾಸ />
+    <ದಿಗ್ವಿಮೋಕ />
+    <ಮುಕ್ತಾಯ />
+  </ವಿಷಯ>
+);
---
+++ RigSandhya/component/Ghatta/Japa/__tests__/PratahGayatriJapa.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ಪ್ರಾತಃಗಾಯತ್ರೀ from '../PratahGayatriJapa';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya', () => 'ವಿಷಯ');
+jest.mock('../../Gayatri/Trihruti', () => 'ತ್ರಿಹೃತಿ');
+jest.mock('../../Sankalpa/PratahJapaSankalpa', () => 'ಪ್ರಾತಃಜಪಸ೦ಕಲ್ಪ');
+jest.mock('../Aavahana', () => 'ಆವಾಹನ');
+jest.mock('../../Gayatri/GayatriMantra', () => 'ಗಾಯತ್ರೀಮ೦ತ್ರ');
+jest.mock('../Karanyaasa', () => 'ಕರನ್ಯಾಸ');
+jest.mock('../Anganyaasa', () => 'ಅ೦ಗನ್ಯಾಸ');
+jest.mock('../Digbhanda', () => 'ದಿಗ್ಬ೦ಧನ');
+jest.mock('../PratahSwaroopa', () => 'ಪ್ರಾತಃಸ್ವರೂಪ');
+jest.mock('../Mantrartha', () => 'ಮ೦ತ್ರಾರ್ಥ');
+jest.mock('../Digvimoka', () => 'ದಿಗ್ವಿಮೋಕ');
+jest.mock('../Muktaya', () => 'ಮುಕ್ತಾಯ');
+ 
+test("ಪ್ರಾತಃಗಾಯತ್ರೀ", () => {
+  expect(Renderer.create(
+    <ಪ್ರಾತಃಗಾಯತ್ರೀ />
+  ).toJSON()).toMatchSnapshot();
+
+}); 
