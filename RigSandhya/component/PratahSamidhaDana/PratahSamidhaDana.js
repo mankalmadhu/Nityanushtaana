2078648d09828912f7fac507ab358b192b2c9080
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-10 18:50:09 +0200

    Pratah Samidha Dana

---
+++ RigSandhya/component/PratahSamidhaDana/PratahSamidhaDana.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import ಲೇಖನ from '../Lekhana/Lekhana';
+import ಪುಟ from '../Puta/Puta';
+import ವಿಷಯ from '../Vishaya/Vishaya';
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta/Ghatta.vinyasa';
+
+import ಆಚಮನೀಯ from '../Ghatta/Aachamaneya/Aachamaneya';
+import ಪ್ರಾತಸ್ಸಮಿಧಾದಾನಸ೦ಕಲ್ಪ from '../Ghatta/Sankalpa/PratahSamidhaDanaSankalpa';
+import ಅಪರಾಧಕ್ಷಮಾಪಣ from '../Ghatta/AparadhaKshamapana/AparadhaKshamapana';
+
+export default ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ = () =>
+
+     (
+      <ಲೇಖನ>
+        <ಪುಟ>
+        <ವಿಷಯ ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ಶೀರ್ಷಿಕೆ}>
+         (ಅಗ್ನಿಯನ್ನು ಪೂರ್ವ/ಉತ್ತರ ಅಭಿಮುಖವಾಗಿ ಇರಿಸಿಕೊ೦ಡು, ಅಗ್ನಿಗೆ ಅಭಿಮುಖವಾಗಿ ಕುಳಿತುಕೊಳ್ಳುವುದು)
+        </ವಿಷಯ>
+        <ಆಚಮನೀಯ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಪ್ರಾತಸ್ಸಮಿಧಾದಾನಸ೦ಕಲ್ಪ/>
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಆಚಮನೀಯ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಅಪರಾಧಕ್ಷಮಾಪಣ />
+        </ಪುಟ>
+      </ಲೇಖನ>
+    );
+  
---
+++ RigSandhya/component/PratahSamidhaDana/__tests__/PratahSamidhaDana.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ from '../PratahSamidhaDana';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Lekhana/Lekhana', () => 'ಲೇಖನ' );
+jest.mock('../../Puta/Puta', () => 'ಪುಟ' );
+jest.mock('../../Ghatta/Ghatta.vinyasa');
+jest.mock('../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+jest.mock('../../Ghatta/Aachamaneya/Aachamaneya', () => 'ಆಚಮನೀಯ' );
+jest.mock('../../Ghatta/Sankalpa/PratahSamidhaDanaSankalpa', () => 'ಪ್ರಾತಸ್ಸಮಿಧಾದಾನಸ೦ಕಲ್ಪ' );
+jest.mock('../../Ghatta/Samarpana/PratahSamarpana', () => 'ಪ್ರಾತಸ್ಸಮರ್ಪಣ' );
+jest.mock('../../Ghatta/AparadhaKshamapana/AparadhaKshamapana', () => 'ಅಪರಾಧಕ್ಷಮಾಪಣ' );
+
+test("ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ", () => {
+  expect(Renderer.create(
+    <ಪ್ರಾತಸ್ಸಮಿಧಾದಾನ />
+  ).toJSON()).toMatchSnapshot();
+
+});
