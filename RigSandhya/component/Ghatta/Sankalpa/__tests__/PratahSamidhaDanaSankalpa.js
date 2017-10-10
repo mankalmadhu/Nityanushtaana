dc94bf62800518ea24c5b7c1cc4736bb0a092a7d
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-10 18:13:02 +0200

    Samidha Dana Sankalpa

---
+++ RigSandhya/component/Ghatta/Sankalpa/__tests__/PratahSamidhaDanaSankalpa.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ಪ್ರಾತಃಸಮಿಧಾದಾನಸ೦ಕಲ್ಪ from '../PratahSamidhaDanaSankalpa';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Gayatri/GayatriMantra', () => 'ಗಾಯತ್ರೀಮ೦ತ್ರ' );
+jest.mock('../../Gayatri/Vyahruti', () => 'ವ್ಯಾಹೃತಿ' );
+jest.mock('../SankalpaPurvanga', () => 'ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ' );
+jest.mock('../SankalpaShershike', () => 'ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ' );
+jest.mock('../Preetyartha', () => 'ಪ್ರೀತ್ಯರ್ಥ' );
+jest.mock('../Oomapo', () => 'ಓಮಾಪೋ' );
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+
+test("ಪ್ರಾತಃಸಮಿಧಾದಾನಸ೦ಕಲ್ಪ", () => {
+
+  expect(Renderer.create(
+    <ಪ್ರಾತಃಸಮಿಧಾದಾನಸ೦ಕಲ್ಪ />
+  ).toJSON()).toMatchSnapshot();
+
+});
