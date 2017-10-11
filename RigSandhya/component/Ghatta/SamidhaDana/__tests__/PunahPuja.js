57ad0673616a161a471a5201bf9e02e916cbbb6f
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-11 10:13:35 +0200

    Punah Puja

---
+++ RigSandhya/component/Ghatta/SamidhaDana/__tests__/PunahPuja.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ಪುನಃಪೂಜಾ from '../PunahPuja';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+jest.mock('../../Gayatri/Trihruti', () => 'ತ್ರಿಹೃತಿ');
+
+test("ಪುನಃಪೂಜಾ", () => {
+   expect(Renderer.create(
+    <ಪುನಃಪೂಜಾ />
+  ).toJSON()).toMatchSnapshot();
+
+});
