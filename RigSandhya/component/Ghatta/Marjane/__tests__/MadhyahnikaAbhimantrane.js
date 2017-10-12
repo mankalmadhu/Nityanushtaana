5d1dbfd6d4f5fe1b272aa787f49d0aca05f01925
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-12 16:19:09 +0200

    Abhimantrane

---
+++ RigSandhya/component/Ghatta/Marjane/__tests__/MadhyahnikaAbhimantrane.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ಮಾಧ್ಯಾಹ್ನಿಕಅಭಿಮ೦ತ್ರಣೆ from '../MadhyahnikaAbhimantrane';
+import Renderer from 'react-test-renderer';
+ 
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+
+test("ಪ್ರಾತಃಅಭಿಮ೦ತ್ರಣೆ", () => {
+  expect(Renderer.create(
+    <ಮಾಧ್ಯಾಹ್ನಿಕಅಭಿಮ೦ತ್ರಣೆ />
+  ).toJSON()).toMatchSnapshot();
+
+});
