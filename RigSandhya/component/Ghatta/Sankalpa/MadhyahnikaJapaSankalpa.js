159af66a3ae36408d882c8eb962c7170b9fdf852
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-12 18:11:59 +0200

    Japa Sankalpa

---
+++ RigSandhya/component/Ghatta/Sankalpa/MadhyahnikaJapaSankalpa.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import ಗಾಯತ್ರೀಮ೦ತ್ರ from '../Gayatri/GayatriMantra';
+import ವ್ಯಾಹೃತಿ from '../Gayatri/Vyahruti';
+import ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ from './SankalpaPurvanga';
+import ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ from './SankalpaShershike';
+import ಓಮಾಪೋ from './Oomapo';
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
+import ಪ್ರೀತ್ಯರ್ಥ from './Preetyartha';
+
+export default ಮಾಧ್ಯಾಹ್ನಿಕಜಪಸ೦ಕಲ್ಪ = () => (
+  <View>
+    <ವಿಷಯ ವಿನ್ಯಾಸ={ಘಟ್ಟವಿನ್ಯಾಸ.ಶೀರ್ಷಿಕೆ}>
+      ಗಾಯತ್ರೀ ಜಪ ಸ೦ಕಲ್ಪ
+    </ವಿಷಯ>
+    <ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ />
+    <ವ್ಯಾಹೃತಿ />
+    <ಗಾಯತ್ರೀಮ೦ತ್ರ />
+    <ಓಮಾಪೋ />
+    <ಪ್ರೀತ್ಯರ್ಥ>
+      ಮಾಧ್ಯಾಹ್ನಿಕ ಸ೦ಧ್ಯಾ೦ಗ ಯಥಾ ಶಕ್ತಿ ಗಾಯತ್ರೀ ಮಹಾಮ೦ತ್ರ ಜಪ೦ ಕರಿಷ್ಯೇ  ॥
+    </ಪ್ರೀತ್ಯರ್ಥ>
+  </View>
+
+);
