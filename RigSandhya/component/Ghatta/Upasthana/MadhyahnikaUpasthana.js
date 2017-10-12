c5254ab86490975d2a15d875f0731364379e8d2a
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-12 17:34:31 +0200

    Madhyahnika Upasthana

---
+++ RigSandhya/component/Ghatta/Upasthana/MadhyahnikaUpasthana.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
+import ಉದುತ್ಯ೦ from './Udutyam';
+import ಚಿತ್ರ೦ದೇವಾನಾ೦ from './ChitramDevanam';
+import ನಮೋಬ್ರಹ್ಮಣೇ from './NamoBramhane';
+import ತಚ್ಚಕ್ಷುಃ from './Tachaskshuhu';
+import ಪ್ರವರ from './Pravara';
+
+export default ಮಾಧ್ಯಾಹ್ನಿಕೌಪಸ್ಥಾನ = () => (
+  <View>
+    <ವಿಷಯ ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ಶೀರ್ಷಿಕೆ} >
+      ಉಪಸ್ಥಾನ
+    </ವಿಷಯ>
+    <ವಿಷಯ ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ಶೀರ್ಷಿಕೆ} >
+      (ಪೂರ್ವಾಭಿಮುಖವಾಗಿ ನಿ೦ತು)
+    </ವಿಷಯ>
+    <ಉದುತ್ಯ೦ />
+    <ಚಿತ್ರ೦ದೇವಾನಾ೦ />
+    <ತಚ್ಚಕ್ಷುಃ />
+    <ನಮೋಬ್ರಹ್ಮಣೇ />
+    <ಪ್ರವರ />
+  </View>
+
+);
---
+++ RigSandhya/component/Ghatta/Upasthana/__tests__/MadhyahnikaUpasthana.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ಮಾಧ್ಯಾಹ್ನಿಕೌಪಸ್ಥಾನ from '../MadhyahnikaUpasthana';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+jest.mock('../Udutyam',()=>'ಉದುತ್ಯ೦');
+jest.mock('../ChitramDevanam',()=>'ಚಿತ್ರ೦ದೇವಾನಾ೦');
+jest.mock('../Tachaskshuhu',()=>'ತಚ್ಚಕ್ಷುಃ');
+jest.mock('../NamoBramhane',()=>'ನಮೋಬ್ರಹ್ಮಣೇ');
+jest.mock('../Pravara',()=>'ಪ್ರವರ');
+
+test("ಮಾಧ್ಯಾಹ್ನಿಕೌಪಸ್ಥಾನ", () => {
+   expect(Renderer.create(
+    <ಮಾಧ್ಯಾಹ್ನಿಕೌಪಸ್ಥಾನ />
+  ).toJSON()).toMatchSnapshot();
+
+});
