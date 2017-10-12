91f59156eab641ec87d505264d4f41a6b5ca1d8a
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-12 16:04:19 +0200

    Madhyahnika Sandhya

---
+++ RigSandhya/component/MadhyahnikaSandhya/MadhyahnikaSandhya.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import ಲೇಖನ from '../Lekhana/Lekhana';
+import ಪುಟ from '../Puta/Puta';
+import ಆಸನಸೂಚನೆ from '../Ghatta/Sochane/AasanaSoochane';
+import ಆಚಮನೀಯ from '../Ghatta/Aachamaneya/Aachamaneya';
+import ಪ್ರೋಕ್ಷಣ from '../Ghatta/Prokshana/Prokshana';
+import ಪ್ರಾತಃಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ from '../Ghatta/Sankalpa/PratahSandhyaSankalpa';
+import ಪ್ರಾತಃಮಾರ್ಜನೆ from '../Ghatta/Marjane/PratahMarjane';
+import ದ್ವಿತೀಯಮಾರ್ಜನೆ from '../Ghatta/DwiteyaMarjane/DwiteyaMarjane';
+import ವಿಸರ್ಜನೆ from '../Ghatta/DwiteyaMarjane/Visarjane';
+import ಪ್ರಾತರ್ರಘ್ಯಸ೦ಕಲ್ಪ from '../Ghatta/Sankalpa/PratahArghyaSankalpa';
+import ಪ್ರಾತರರ್ಘ್ಯ from '../Ghatta/ArghyaPradana/PratahArghyaPradana';
+import ಪ್ರಾತಃಗಾಯತ್ರೀ from '../Ghatta/Japa/PratahGayatriJapa';
+import ಪ್ರಾತರುಪಸ್ಥಾನ from '../Ghatta/Upasthana/PratahUpasthana';
+import ಪ್ರಾತಸ್ಸಮರ್ಪಣ from '../Ghatta/Samarpana/PratahSamarpana';
+import ಅಪರಾಧಕ್ಷಮಾಪಣ from '../Ghatta/AparadhaKshamapana/AparadhaKshamapana';
+
+export default ಮಾಧ್ಯಾಹ್ನಿಕಸ೦ಧ್ಯಾ = () =>
+ (
+
+      <ಲೇಖನ>
+        <ಪುಟ>
+          <ಆಸನಸೂಚನೆ />
+          <ಆಚಮನೀಯ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಪ್ರೋಕ್ಷಣ />
+          <ಆಚಮನೀಯ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಪ್ರಾತಃಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಪ್ರಾತಃಮಾರ್ಜನೆ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಆಚಮನೀಯ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ದ್ವಿತೀಯಮಾರ್ಜನೆ />
+          <ವಿಸರ್ಜನೆ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಆಚಮನೀಯ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಪ್ರಾತರ್ರಘ್ಯಸ೦ಕಲ್ಪ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಪ್ರಾತರರ್ಘ್ಯ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಪ್ರಾತಃಗಾಯತ್ರೀ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಪ್ರಾತರುಪಸ್ಥಾನ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಆಚಮನೀಯ />
+          <ಪ್ರಾತಸ್ಸಮರ್ಪಣ />
+        </ಪುಟ>
+        <ಪುಟ>
+          <ಅಪರಾಧಕ್ಷಮಾಪಣ />
+        </ಪುಟ>
+      </ಲೇಖನ>
+ )
