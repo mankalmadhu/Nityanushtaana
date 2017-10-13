1ca138bd554b19d7d14e8e9db39c9cc8d14453b2
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-13 09:28:28 +0200

    Sayam Sandhya Sankalpa

---
+++ RigSandhya/component/Ghatta/Sankalpa/__tests__/SayamSandhyaSankalpa.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ಸಾಯ೦ಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ from '../SayamSandhyaSankalpa';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Gayatri/GayatriMantra', () => 'ಗಾಯತ್ರೀಮ೦ತ್ರ' );
+jest.mock('../../Gayatri/Vyahruti', () => 'ವ್ಯಾಹೃತಿ' );
+jest.mock('../SankalpaPurvanga', () => 'ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ' );
+jest.mock('../SankalpaShershike', () => 'ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ' );
+jest.mock('../Preetyartha', () => 'ಪ್ರೀತ್ಯರ್ಥ' );
+jest.mock('../Oomapo', () => 'ಓಮಾಪೋ' );
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+
+test("ಸಾಯ೦ಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ", () => {
+
+  expect(Renderer.create(
+    <ಸಾಯ೦ಸ೦ಧ್ಯಾಸ೦ಕಲ್ಪ />
+  ).toJSON()).toMatchSnapshot();
+
+});
