15b578fa9abb0d356f43793e93a58d8a5011f0cf
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-18 14:14:59 +0200

    Arghya Pradana

---
+++ RigSandhya/component/Ghatta/ArghyaPradana/__tests__/SayamArghyaPradana.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ಸಾಯಮರ್ಘ್ಯ from '../SayamArghyaPradana';
+import Renderer from 'react-test-renderer';
+ 
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+jest.mock('../../Gayatri/Trihruti',()=>'ತ್ರಿಹೃತಿ');
+jest.mock('../../Gayatri/GayatriMantra',()=>'ಗಾಯತ್ರೀಮ೦ತ್ರ');
+jest.mock('../../Sankalpa/Oomapo',()=>'ಓಮಾಪೋ');
+jest.mock('../KalaTeetaArgyaPradana',()=>'ಕಾಲಾತೀತಅರ್ಘ್ಯ');
+
+test("ಸಾಯಮರ್ಘ್ಯ", () => {
+   expect(Renderer.create(
+    <ಸಾಯಮರ್ಘ್ಯ />
+  ).toJSON()).toMatchSnapshot();
+
+});
