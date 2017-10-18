a65cb56f02d140ff74f285e601b10ef1c3cf5319
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-18 15:19:34 +0200

    Sayam Samidha Sankalpa

---
+++ RigSandhya/component/Ghatta/Sankalpa/SayamSamidhaDanaSankalpa.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import ಗಾಯತ್ರೀಮ೦ತ್ರ from '../Gayatri/GayatriMantra';
+import ವ್ಯಾಹೃತಿ from '../Gayatri/Vyahruti';
+import ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ from './SankalpaPurvanga';
+import ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ from './SankalpaShershike';
+import ಓಮಾಪೋ from './Oomapo';
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
+import ಪ್ರೀತ್ಯರ್ಥ from './Preetyartha';
+
+export default ಸಾಯ೦ಸಮಿಧಾದಾನಸ೦ಕಲ್ಪ = () => (
+  <View>
+    <ಸ೦ಕಲ್ಪಶೀರ್ಷಿಕೆ />
+    <ಸ೦ಕಲ್ಪಪೂರ್ವಾ೦ಗ />
+    <ವ್ಯಾಹೃತಿ />
+    <ಗಾಯತ್ರೀಮ೦ತ್ರ />
+    <ಓಮಾಪೋ />
+    <ಪ್ರೀತ್ಯರ್ಥ>
+      ಸಾಯ೦ ಸಮಿಧಾದಾನ೦ ಕರಿಷ್ಯೇ ॥
+    </ಪ್ರೀತ್ಯರ್ಥ>
+  </View>
+
+);
