42536328bf7ef8ff4a633b3dece5204016d5f555
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-18 14:57:45 +0200

    Sayam Upasthana

---
+++ RigSandhya/component/Ghatta/Upasthana/SayamUpasthana.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
+import ವಿಷಯ from '../../Vishaya/Vishaya';
+import ಜಾತವೇದಸೇ from './JaataVedase';
+import ತಚ್ಛ೦ಯೋ from './Tachayo';
+import ನಮೋಬ್ರಹ್ಮಣೇ from './NamoBramhane';
+import ಸಾಯ೦ದಿಶಾನಮಸ್ಕಾರ from './SayamDishaNamskara';
+import ದೇವತಾನಮಸ್ಕಾರ from './DevataNamskara';
+import ನಿರ್ಗಮನ from './Niragamana';
+import ಪ್ರವರ from './Pravara';
+import ಆಸತ್ಯಲೋಕಾತ್ from './AasatyaLokaat';
+
+export default ಸಾಯಮುಪಸ್ಥಾನ = () => (
+  <View>
+    <ವಿಷಯ ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ಶೀರ್ಷಿಕೆ} >
+      ಉಪಸ್ಥಾನ
+    </ವಿಷಯ>
+    <ವಿಷಯ ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ಶೀರ್ಷಿಕೆ} >
+      (ಪಶ್ಚಿಮಾಭಿಮುಖವಾಗಿ ನಿ೦ತು)
+    </ವಿಷಯ>
+    <ಜಾತವೇದಸೇ />
+    <ತಚ್ಛ೦ಯೋ />
+    <ನಮೋಬ್ರಹ್ಮಣೇ />
+    <ವಿಷಯ ವಿನ್ಯಾಸ = {ಘಟ್ಟವಿನ್ಯಾಸ.ವಿನಿಯೋಗ} >
+      (ಮೂರು ಬಾರಿ)
+    </ವಿಷಯ>
+    <ಸಾಯ೦ದಿಶಾನಮಸ್ಕಾರ />
+    <ದೇವತಾನಮಸ್ಕಾರ />
+    <ನಿರ್ಗಮನ />
+    <ನಮೋಬ್ರಹ್ಮಣೇ />
+    <ಪ್ರವರ />
+    <ಆಸತ್ಯಲೋಕಾತ್ />
+  </View>
+
+);
---
+++ RigSandhya/component/Ghatta/Upasthana/__tests__/SayamUpasthana.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ಸಾಯಮುಪಸ್ಥಾನ from '../SayamUpasthana';
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../Ghatta.vinyasa');
+jest.mock('../../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+jest.mock('../JaataVedase',()=>'ಜಾತವೇದಸೇ');
+jest.mock('../Tachayo',()=>'ತಚ್ಛ೦ಯೋ');
+jest.mock('../NamoBramhane',()=>'ನಮೋಬ್ರಹ್ಮಣೇ');
+jest.mock('../SayamDishaNamskara',()=>'ಸಾಯ೦ದಿಶಾನಮಸ್ಕಾರ');
+jest.mock('../DevataNamskara',()=>'ದೇವತಾನಮಸ್ಕಾರ');
+jest.mock('../Niragamana',()=>'ನಿರ್ಗಮನ');
+jest.mock('../Pravara',()=>'ಪ್ರವರ');
+jest.mock('../AasatyaLokaat',()=>'ಆಸತ್ಯಲೋಕಾತ್');
+
+test("ಸಾಯಮುಪಸ್ಥಾನ", () => {
+   expect(Renderer.create(
+    <ಸಾಯಮುಪಸ್ಥಾನ />
+  ).toJSON()).toMatchSnapshot();
+
+});
