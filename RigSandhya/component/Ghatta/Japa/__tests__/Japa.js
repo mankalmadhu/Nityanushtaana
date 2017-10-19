ee7cc301a1321b84f65b1b71a9d59692c11c00c8
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-19 09:57:12 +0200

    Japa

--- RigSandhya/component/Ghatta/Japa/__tests__/Japa.js
+++ RigSandhya/component/Ghatta/Japa/__tests__/Japa.js
@@ -5,6 +5,7 @@ import Renderer from 'react-test-renderer';
 jest.mock('../../Ghatta.vinyasa');
 jest.mock('../../../Vishaya/Vishaya', () => 'ವಿಷಯ');
 jest.mock('../../Gayatri/Trihruti', () => 'ತ್ರಿಹೃತಿ');
+jest.mock('../../Gayatri/GayatriMantra', () => 'ಗಾಯತ್ರೀಮ೦ತ್ರ');
 
 test("ಜಪ", () => {
   expect(Renderer.create(
