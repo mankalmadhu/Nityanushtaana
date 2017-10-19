e88ad87d2b6bdba8f1dc850d7f03811b7c28cbfb
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-19 10:02:25 +0200

    Japa

--- RigSandhya/component/Ghatta/Japa/SayamGayatriJapa.js
+++ RigSandhya/component/Ghatta/Japa/SayamGayatriJapa.js
@@ -12,6 +12,7 @@ import ಅ೦ಗನ್ಯಾಸ from './Anganyaasa';
 import ದಿಗ್ಬ೦ಧನ from './Digbhanda';
 import ಸಾಯ೦ಸ್ವರೂಪ from './SayamSwaroopa';
 import ಮ೦ತ್ರಾರ್ಥ from './Mantrartha';
+import ಜಪ from './Japa';
 import ದಿಗ್ವಿಮೋಕ from './Digvimoka';
 import ಮುಕ್ತಾಯ from './Muktaya';
 
@@ -29,12 +30,7 @@ export default ಸಾಯ೦ಗಾಯತ್ರೀ = () => (
     <ದಿಗ್ಬ೦ಧನ />
     <ಸಾಯ೦ಸ್ವರೂಪ />
     <ಮ೦ತ್ರಾರ್ಥ />
-    <ತ್ರಿಹೃತಿ>
-      <ಗಾಯತ್ರೀಮ೦ತ್ರ />
-    </ತ್ರಿಹೃತಿ>
-    <ವಿಷಯ ವಿನ್ಯಾಸ={ಘಟ್ಟವಿನ್ಯಾಸ.ವಿನಿಯೋಗ}>
-      ತತ್ಸಬ್ರಹ್ಮಾರ್ಪಣಮಸ್ತು
-    </ವಿಷಯ>
+    <ಜಪ />
     <ಕರನ್ಯಾಸ />
     <ಅ೦ಗನ್ಯಾಸ />
     <ದಿಗ್ವಿಮೋಕ />
