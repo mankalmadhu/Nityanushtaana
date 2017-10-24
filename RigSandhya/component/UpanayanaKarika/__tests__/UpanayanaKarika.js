9ca167818b3e07e5de065012c5ff1860ce85417b
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-24 10:16:34 +0200

    Upanayana Karika

--- RigSandhya/component/UpanayanaKarika/__tests__/UpanayanaKarika.js
+++ RigSandhya/component/UpanayanaKarika/__tests__/UpanayanaKarika.js
@@ -4,8 +4,7 @@ import Renderer from 'react-test-renderer';
 
 jest.mock('../../Lekhana/Lekhana', () => 'ಲೇಖನ' );
 jest.mock('../../Puta/Puta', () => 'ಪುಟ' );
-jest.mock('../../Ghatta/Ghatta.vinyasa');
-jest.mock('../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+jest.mock('../../Ghatta/UpanayanaKarikaMantra/UpanayanaKarikaMantra',()=>'ಉಪನಯನಕಾರಿಕಾಮ೦ತ್ರ');
 
 test("ಉಪನಯನಕಾರಿಕಾ", () => {
   expect(Renderer.create(
