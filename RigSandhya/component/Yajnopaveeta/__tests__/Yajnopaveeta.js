b4efe680b675ec645aefab6d80d3ef4ae9480ae6
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-24 10:30:57 +0200

    Yajnopaveeta

--- RigSandhya/component/Yajnopaveeta/__tests__/Yajnopaveeta.js
+++ RigSandhya/component/Yajnopaveeta/__tests__/Yajnopaveeta.js
@@ -4,8 +4,8 @@ import Renderer from 'react-test-renderer';
 
 jest.mock('../../Lekhana/Lekhana', () => 'ಲೇಖನ' );
 jest.mock('../../Puta/Puta', () => 'ಪುಟ' );
-jest.mock('../../Ghatta/Ghatta.vinyasa');
-jest.mock('../../Vishaya/Vishaya',()=>'ವಿಷಯ');
+jest.mock('../../Ghatta/YajnopavetaMantra/YajnopavetaDharana',()=>'ಯಜ್ಞೋಪವೀತಧಾರಣ');
+jest.mock('../../Ghatta/YajnopavetaMantra/YajnopavetaVisarjana',()=>'ಯಜ್ಞೋಪವೀತವಿಸರ್ಜನ');
 
 test("ಯಜ್ಞೋಪವೀತ", () => {
   expect(Renderer.create(
