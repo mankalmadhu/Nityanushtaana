d56678b6a7a2fa16e7c56216fa79e481a4571704
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-24 20:30:12 +0200

    ghatta vinyasa del

--- RigSandhya/component/Ghatta/__mocks__/Ghatta.vinyasa.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ghattaVinyasa = {
-   
-  ವಿನಿಯೋಗ :'ವಿನಿಯೋಗ',
-  ಶೀರ್ಷಿಕೆ : 'ಶೀರ್ಷಿಕೆ',
-  ಪೂರ್ವಾ೦ಗ :'ಪೂರ್ವಾ೦ಗ'
-  
-};
--- RigSandhya/component/Ghatta/__tests__/Ghatta.vinyasa.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ಘಟ್ಟವಿನ್ಯಾಸ from '../Ghatta.vinyasa';
-
-test("Ghatta Vinyasa" , () => {
-  expect(<div>{ಘಟ್ಟವಿನ್ಯಾಸ}</div>).toMatchSnapshot();
-});
