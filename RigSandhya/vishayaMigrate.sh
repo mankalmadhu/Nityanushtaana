5f5153a53d985bb097ae136d6d71219dbd1fa786
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-24 20:10:15 +0200

    more purvanga

--- RigSandhya/vishayaMigrate.sh
+++ RigSandhya/vishayaMigrate.sh
@@ -1,11 +1,11 @@
 cd component/Ghatta
-fileNames=($(grep -rl '<.*ವಿಷಯ.*ವಿನ್ಯಾಸ.*=.*{ಘಟ್ಟವಿನ್ಯಾಸ.ಶೀರ್ಷಿಕೆ}.*>' --exclude-dir={'__tests__'}))
+fileNames=($(grep -rl '<.*ವಿಷಯ.*ವಿನ್ಯಾಸ.*=.*{ಘಟ್ಟವಿನ್ಯಾಸ.ಪೂರ್ವಾ೦ಗ}.*>' --exclude-dir={'__tests__'}))
 for fileName in "${fileNames[@]}"
   do
-    sed -i "4i import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';" $fileName
-    sed -i "s/<.*ವಿಷಯ.*ವಿನ್ಯಾಸ.*=.*{ಘಟ್ಟವಿನ್ಯಾಸ.ಶೀರ್ಷಿಕೆ}.*>/<ಶೀರ್ಷಿಕೆ>/g" $fileName
+    sed -i "4i import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';" $fileName
+    sed -i "s/<.*ವಿಷಯ.*ವಿನ್ಯಾಸ.*=.*{ಘಟ್ಟವಿನ್ಯಾಸ.ಪೂರ್ವಾ೦ಗ}.*>/<ಪೂರ್ವಾ೦ಗ>/g" $fileName
     dir=$(dirname "${fileName}")
     fileBase=$(basename "${fileName}")
-    sed -i "7i jest.mock('../../../Vishaya/Sheershike',()=>'ಶೀರ್ಷಿಕೆ');" $dir/'__tests__'/$fileBase
+    sed -i "7i jest.mock('../../../Vishaya/Purvanga',()=>'ಪೂರ್ವಾ೦ಗ');" $dir/'__tests__'/$fileBase
   done
 cd ../..
