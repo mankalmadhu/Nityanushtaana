1cc4be01f6774498c9e41537ccb58c6094f1d921
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-25 10:03:27 +0200

    Lekhana Updated

--- RigSandhya/component/Lekhana/Lekhana.js
+++ RigSandhya/component/Lekhana/Lekhana.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {ViewPagerAndroid } from 'react-native';
+import {View } from 'react-native';
+import Swiper from 'react-native-swiper';
 import lekhanaVinyasa from './Lekhana.vinyasa';
 
 export default ಲೇಖನ = (props) => (
-    <ViewPagerAndroid style={lekhanaVinyasa.avarana} initialPage={0}>
+    <Swiper style={lekhanaVinyasa.avarana} 
+      paginationStyle={{backgroundColor:'#595959',height:30, bottom:0}}
+      dotColor='#ffffff'
+      activeDotColor='#3bdde5'
+      >
       {props.children}
-    </ViewPagerAndroid>
+    </Swiper>
 );
 
--- RigSandhya/component/Lekhana/__tests__/Lekhana.js
+++ RigSandhya/component/Lekhana/__tests__/Lekhana.js
@@ -4,6 +4,7 @@ import ಲೇಖನ from '../Lekhana';
 
 import Renderer from 'react-test-renderer';
 
+jest.mock('react-native-swiper',()=>'Swiper');
 test("ಲೇಖನ" , () => {
 
 expect(Renderer.create(
