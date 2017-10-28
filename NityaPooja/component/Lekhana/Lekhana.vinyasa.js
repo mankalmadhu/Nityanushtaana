43f5ce0ee09f7127d801fa3d0aa00740ae60b202
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-28 12:06:06 +0200

    Uneeded Style in lekhana

--- NityaPooja/component/Lekhana/Lekhana.vinyasa.js
+++ NityaPooja/component/Lekhana/Lekhana.vinyasa.js
@@ -2,11 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export default lekhanaVinyasa = StyleSheet.create({
   avarana: {
-    flex:1,
-    padding: 3,
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: '#333333'
+       backgroundColor: '#333333'
   }
     
 });
