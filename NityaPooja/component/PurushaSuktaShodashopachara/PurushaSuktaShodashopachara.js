2bc0938c9b38a1839bb1733ff5701f2a4c73a740
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-28 10:45:00 +0200

    Update Puja

--- NityaPooja/component/PurushaSuktaShodashopachara/PurushaSuktaShodashopachara.js
+++ NityaPooja/component/PurushaSuktaShodashopachara/PurushaSuktaShodashopachara.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ಲೇಖನ from '../Lekhana/Lekhana';
 import ಪುಟ from '../Puta/Puta';
 
+import ಆಚಮನೀಯ from '../Bhandara/Aachamaneya/Aachamaneya';
+import ಆಚಮನೀಯಸೂಚನೆ from '../Bhandara/Sochane/AachaManeyaSoochane';
+import ದೇವಪೂಜಾಆಸನಸೂಚನೆ from '../Bhandara/Sochane/DevaPujaAasnaSochane';
 import ಶುಕ್ಲಾ೦ಬರಧರ೦ from '../Bhandara/Shuklambaradharam/Shuklambaradharam';
 
 export default ಪುರುಷಸೂಕ್ತಶೋಢಷೋಪಚಾರ = () =>
@@ -10,9 +13,14 @@ export default ಪುರುಷಸೂಕ್ತಶೋಢಷೋಪಚಾರ = () =>
   (
     <ಲೇಖನ>
       <ಪುಟ>
-       <ಶುಕ್ಲಾ೦ಬರಧರ೦/>
+        <ಆಚಮನೀಯಸೂಚನೆ />
+        <ಆಚಮನೀಯ />
       </ಪುಟ>
 
+      <ಪುಟ>
+        <ದೇವಪೂಜಾಆಸನಸೂಚನೆ />
+        <ಶುಕ್ಲಾ೦ಬರಧರ೦ />
+      </ಪುಟ>
     </ಲೇಖನ>
   );
 
--- NityaPooja/component/PurushaSuktaShodashopachara/__tests__/PurushaSuktaShodashopachara.js
+++ NityaPooja/component/PurushaSuktaShodashopachara/__tests__/PurushaSuktaShodashopachara.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ಪುರುಷಸೂಕ್ತಶೋಢಷೋಪಚಾರ from '../PurushaSuktaShodashopachara';
 import Renderer from 'react-test-renderer';
 
+
 jest.mock('../../Lekhana/Lekhana', () => 'ಲೇಖನ' );
 jest.mock('../../Puta/Puta', () => 'ಪುಟ' );
+jest.mock('../../Bhandara/Aachamaneya/Aachamaneya',()=>'ಆಚಮನೀಯ');
+jest.mock('../../Bhandara/Sochane/AachaManeyaSoochane',()=>'ಆಚಮನೀಯಸೂಚನೆ');
+jest.mock('../../Bhandara/Sochane/DevaPujaAasnaSochane',()=>'ದೇವಪೂಜಾಆಸನಸೂಚನೆ');
 jest.mock('../../Bhandara/Shuklambaradharam/Shuklambaradharam',()=>'ಶುಕ್ಲಾ೦ಬರಧರ೦');
 
 test("ಪುರುಷಸೂಕ್ತಶೋಢಷೋಪಚಾರ", () => {
