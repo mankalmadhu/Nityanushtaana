704b43b50ee9eeacc718e9334b91199545341655
Madhu Mankal <mankalmadhu@gmail.com>
2017-10-29 11:45:01 +0100

    Lekhana Updated

--- NityaPooja/component/Lekhana/Lekhana.js
+++ NityaPooja/component/Lekhana/Lekhana.js
@@ -2,16 +2,11 @@ import React from 'react';
 import {View } from 'react-native';
 import Swiper from 'react-native-swiper';
 import lekhanaVinyasa from './Lekhana.vinyasa';
-import ಲೇಖನಶೀರ್ಷಿಕೆ from '../LekhanaSheershike/LekhanaShershike';
 
 export default class ಲೇಖನ extends React.Component {
 
   render(){
     return (
-      <View style={{flex:1}}>
-      <ಲೇಖನಶೀರ್ಷಿಕೆ>
-        {this.props.title}
-      </ಲೇಖನಶೀರ್ಷಿಕೆ>
       <Swiper style={lekhanaVinyasa.avarana} 
       paginationStyle={{backgroundColor:'#595959',height:30, bottom:0}}
       dotColor='#ffffff'
@@ -19,7 +14,7 @@ export default class ಲೇಖನ extends React.Component {
       >
       {this.props.children}
     </Swiper>
-    </View>
+
     );
   }
   
