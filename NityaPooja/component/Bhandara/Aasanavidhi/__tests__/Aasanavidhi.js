0fc53acd35cc3cd9d951e0ba286c55d3f3927b79
Madhu Mankal <mankalmadhu@gmail.com>
2017-11-24 22:01:12 +0100

    Aasana Vidhi

---
+++ NityaPooja/component/Bhandara/Aasanavidhi/__tests__/Aasanavidhi.js
@@ -0,0 +1,19 @@
+import 'react-native';
+import React from 'react';
+import ಆಸನವಿಧಿ from '../Aasanavidhi';
+
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../../Avarana/Avarana',()=>'ಆವರಣ');
+jest.mock('../../../Vishaya/Sheershike',()=>'ಶೀರ್ಷಿಕೆ');
+jest.mock('../../../Vishaya/Purvanga',()=>'ಪೂರ್ವಾ೦ಗ');
+jest.mock('../../../Vishaya/Viniyoga',()=>'ವಿನಿಯೋಗ');
+jest.mock('../../../Vishaya/Vidhana',()=>'ವಿಧಾನ');
+
+test("ಆಸನವಿಧಿ" , () => {
+
+expect(Renderer.create(
+    <ಆಸನವಿಧಿ />
+).toJSON()).toMatchSnapshot();
+
+});
