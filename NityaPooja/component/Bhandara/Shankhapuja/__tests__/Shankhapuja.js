ecaf81feb17e0bbeecd5e02b39ac2068d609ee3f
Madhu Mankal <mankalmadhu@gmail.com>
2017-11-28 22:04:58 +0100

    Shankhapuja

---
+++ NityaPooja/component/Bhandara/Shankhapuja/__tests__/Shankhapuja.js
@@ -0,0 +1,19 @@
+import 'react-native';
+import React from 'react';
+import ಶ೦ಖಪೂಜಾ from '../Shankhapuja';
+
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../../Avarana/Avarana',()=>'ಆವರಣ');
+jest.mock('../../../Vishaya/Sheershike',()=>'ಶೀರ್ಷಿಕೆ');
+jest.mock('../../../Vishaya/Purvanga',()=>'ಪೂರ್ವಾ೦ಗ');
+jest.mock('../../../Vishaya/Viniyoga',()=>'ವಿನಿಯೋಗ');
+jest.mock('../../../Vishaya/Vidhana',()=>'ವಿಧಾನ');
+
+test("ಶ೦ಖಪೂಜಾ" , () => {
+
+expect(Renderer.create(
+    <ಶ೦ಖಪೂಜಾ />
+).toJSON()).toMatchSnapshot();
+
+});
