78cabe4aa0735e0fb95373201c9c51ba27c6be02
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-02 20:05:30 +0100

    Aasana

---
+++ NityaPooja/component/Bhandara/Aasana/Aasana.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
+import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
+import ಆವರಣ from '../../Avarana/Avarana';
+import ವಿಧಾನ from '../../Vishaya/Vidhana';
+
+export default ಆಸನ = () => (
+    <ಆವರಣ>
+    <ಶೀರ್ಷಿಕೆ>
+      ಆಸನ
+    </ಶೀರ್ಷಿಕೆ>
+    <ವಿನಿಯೋಗ >
+     ಪುರು॑ಷ ಏ॒ವೇದꣳ ಸರ್ವಮ್᳚ । ಯದ್ಭೂ॒ತಂ ಯಚ್ಚ॒ ಭವ್ಯಮ್᳚ । ಉ॒ತಾಮೃ॑ತ॒ತ್ವಸ್ಯೇಶಾ॑ನಃ । ಯದನ್ನೇ॑ನಾತಿ॒ರೋಹ॑ತಿ ।
+     </ವಿನಿಯೋಗ>
+    <ಪೂರ್ವಾ೦ಗ>
+      ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಆಸನ೦ ಕಲ್ಪಯಾಮಿ ॥
+    </ಪೂರ್ವಾ೦ಗ>
+
+    </ಆವರಣ>
+);
