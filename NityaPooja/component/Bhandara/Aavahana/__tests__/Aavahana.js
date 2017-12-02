b2fad94063bfe708184f1305a3d7247ae188d182
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-02 19:54:41 +0100

    Aavahana

---
+++ NityaPooja/component/Bhandara/Aavahana/__tests__/Aavahana.js
@@ -0,0 +1,19 @@
+import 'react-native';
+import React from 'react';
+import ಧ್ಯಾನಆವಾಹನ from '../Aavahana';
+
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../../Avarana/Avarana',()=>'ಆವರಣ');
+jest.mock('../../../Vishaya/Sheershike',()=>'ಶೀರ್ಷಿಕೆ');
+jest.mock('../../../Vishaya/Purvanga',()=>'ಪೂರ್ವಾ೦ಗ');
+jest.mock('../../../Vishaya/Viniyoga',()=>'ವಿನಿಯೋಗ');
+jest.mock('../../../Vishaya/Vidhana',()=>'ವಿಧಾನ');
+
+test("ಧ್ಯಾನಆವಾಹನ" , () => {
+
+expect(Renderer.create(
+    <ಧ್ಯಾನಆವಾಹನ />
+).toJSON()).toMatchSnapshot();
+
+});
