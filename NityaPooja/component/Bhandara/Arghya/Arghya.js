a56bdf0d94c785ab6eab8958a8a053d00638268f
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-02 20:34:48 +0100

    Arghya

---
+++ NityaPooja/component/Bhandara/Arghya/Arghya.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
+import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
+import ಆವರಣ from '../../Avarana/Avarana';
+import ವಿಧಾನ from '../../Vishaya/Vidhana';
+
+export default ಅರ್ಘ್ಯ = () => (
+    <ಆವರಣ>
+    <ಶೀರ್ಷಿಕೆ>
+      ಅರ್ಘ್ಯ
+    </ಶೀರ್ಷಿಕೆ>
+    <ವಿನಿಯೋಗ >
+    ತ್ರಿ॒ಪಾದೂ॒ರ್ಧ್ವ ಉದೈ॒ತ್ಪುರು॑ಷಃ । ಪಾದೋ᳚ಽಸ್ಯೇ॒ಹಾಭ॑ವಾ॒ತ್ಪುನಃ॑ ।
+    
+    ತತೋ॒ ವಿಷ್ವಙ್-ವ್ಯ॑ಕ್ರಾಮತ್ । ಸಾ॒ಶ॒ನಾ॒ನ॒ಶ॒ನೇ ಅ॒ಭಿ ।
+
+    </ವಿನಿಯೋಗ>
+    <ಪೂರ್ವಾ೦ಗ>
+      ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಹಸ್ತೇಷು ಅರ್ಘ್ಯಮರ್ಘ್ಯ೦ ಸಮರ್ಪಯಾಮಿ ॥
+    </ಪೂರ್ವಾ೦ಗ>
+
+    </ಆವರಣ>
+);
