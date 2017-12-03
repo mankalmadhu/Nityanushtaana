ef40884794083396dca43af50a7992ce0146c68b
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-03 21:36:18 +0100

    Achamaneya

---
+++ NityaPooja/component/Bhandara/UpacharaAchamaneya/UpacharaAchamaneya.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
+import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
+import ಆವರಣ from '../../Avarana/Avarana';
+import ವಿಧಾನ from '../../Vishaya/Vidhana';
+
+export default ಉಪಚಾರಆಚಮನೀಯ = () => (
+    <ಆವರಣ>
+    <ಶೀರ್ಷಿಕೆ>
+      ಆಚಮನೀಯ
+    </ಶೀರ್ಷಿಕೆ>
+    <ವಿನಿಯೋಗ >
+      ತಸ್ಮಾ᳚ದ್ವಿ॒ರಾಡ॑ಜಾಯತ । ವಿ॒ರಾಜೋ॒ ಅಧಿ॒ ಪೂರು॑ಷಃ । ಸ ಜಾ॒ತೋ ಅತ್ಯ॑ರಿಚ್ಯತ । ಪ॒ಶ್ಚಾದ್ಭೂಮಿ॒ಮಥೋ॑ ಪು॒ರಃ ।
+ 
+     </ವಿನಿಯೋಗ>
+    <ಪೂರ್ವಾ೦ಗ>
+      ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಮುಖೇ ಆಚಮನೀಯ೦ ಆಚಮನೀಯ೦ ಸಮರ್ಪಯಾಮಿ ॥
+    </ಪೂರ್ವಾ೦ಗ>
+
+    </ಆವರಣ>
+);
---
+++ NityaPooja/component/Bhandara/UpacharaAchamaneya/__tests__/UpacharaAchamaneya.js
@@ -0,0 +1,19 @@
+import 'react-native';
+import React from 'react';
+import ಉಪಚಾರಆಚಮನೀಯ from '../UpacharaAchamaneya';
+
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../../Avarana/Avarana',()=>'ಆವರಣ');
+jest.mock('../../../Vishaya/Sheershike',()=>'ಶೀರ್ಷಿಕೆ');
+jest.mock('../../../Vishaya/Purvanga',()=>'ಪೂರ್ವಾ೦ಗ');
+jest.mock('../../../Vishaya/Viniyoga',()=>'ವಿನಿಯೋಗ');
+jest.mock('../../../Vishaya/Vidhana',()=>'ವಿಧಾನ');
+
+test("ಉಪಚಾರಆಚಮನೀಯ" , () => {
+
+expect(Renderer.create(
+    <ಉಪಚಾರಆಚಮನೀಯ />
+).toJSON()).toMatchSnapshot();
+
+});
