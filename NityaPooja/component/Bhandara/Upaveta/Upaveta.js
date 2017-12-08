a36818edcc9c33d8e35063970c71e46eb9b9dc89
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-08 22:08:24 +0100

    Upaveta

---
+++ NityaPooja/component/Bhandara/Upaveta/Upaveta.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
+import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
+import ಆವರಣ from '../../Avarana/Avarana';
+import ವಿಧಾನ from '../../Vishaya/Vidhana';
+ 
+export default ಉಪವೀತ = () => (
+    <ಆವರಣ>
+    <ಶೀರ್ಷಿಕೆ>
+      ಉಪವೀತ
+    </ಶೀರ್ಷಿಕೆ>
+    <ವಿನಿಯೋಗ >
+ 
+    ತಂ ಯ॒ಜ್ಞಂ ಬ॒ರ್ಹಿಷಿ॒ ಪ್ರೌಕ್ಷನ್॑ । ಪುರು॑ಷಂಜಾ॒ತಮ॑ಗ್ರ॒ತಃ । ತೇನ॑ ದೇ॒ವಾ ಅಯ॑ಜಂತ । ಸಾ॒ಧ್ಯಾ ಋಷ॑ಯಶ್ಚ॒ಯೇ ।
+
+     </ವಿನಿಯೋಗ>
+    <ಪೂರ್ವಾ೦ಗ>
+      ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಉಪವೀತ೦ ಸಮರ್ಪಯಾಮಿ ॥
+    </ಪೂರ್ವಾ೦ಗ>
+
+    </ಆವರಣ>
+);
