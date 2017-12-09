22a39f1240dba895d61645977b388a939f4f6832
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-09 20:17:47 +0100

    Gandha

---
+++ NityaPooja/component/Bhandara/Gandha/Gandha.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
+import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
+import ಆವರಣ from '../../Avarana/Avarana';
+import ವಿಧಾನ from '../../Vishaya/Vidhana';
+ 
+export default ಉಪವೀತ = () => (
+    <ಆವರಣ>
+    <ಶೀರ್ಷಿಕೆ>
+      ಉಪವೀತ
+    </ಶೀರ್ಷಿಕೆ>
+    <ವಿನಿಯೋಗ >
+ 
+    ತಸ್ಮಾ᳚ದ್ಯ॒ಜ್ಞಾಥ್ಸ॑ರ್ವ॒ಹುತಃ॑ । ಸಂಭೃ॑ತಂ ಪೃಷದಾ॒ಜ್ಯಮ್ । ಪ॒ಶೂಗ್-ಸ್ತಾಗ್-ಶ್ಚ॑ಕ್ರೇ ವಾಯ॒ವ್ಯಾನ್॑ । ಆ॒ರ॒ಣ್ಯಾನ್ಗ್ರಾ॒ಮ್ಯಾಶ್ಚ॒ ಯೇ ।
+
+     </ವಿನಿಯೋಗ>
+    <ಪೂರ್ವಾ೦ಗ>
+      ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ದಿವ್ಯಪರಿಮಳಗ೦ಧ೦ ಸಮರ್ಪಯಾಮಿ ॥
+    </ಪೂರ್ವಾ೦ಗ>
+
+    </ಆವರಣ>
+);
---
+++ NityaPooja/component/Bhandara/Gandha/__tests__/Gandha.js
@@ -0,0 +1,19 @@
+import 'react-native';
+import React from 'react';
+import ಗ೦ಧ from '../Gandha';
+ 
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../../Avarana/Avarana',()=>'ಆವರಣ');
+jest.mock('../../../Vishaya/Sheershike',()=>'ಶೀರ್ಷಿಕೆ');
+jest.mock('../../../Vishaya/Purvanga',()=>'ಪೂರ್ವಾ೦ಗ');
+jest.mock('../../../Vishaya/Viniyoga',()=>'ವಿನಿಯೋಗ');
+jest.mock('../../../Vishaya/Vidhana',()=>'ವಿಧಾನ');
+
+test("ಗ೦ಧ" , () => {
+
+expect(Renderer.create(
+    <ಗ೦ಧ />
+).toJSON()).toMatchSnapshot();
+
+});
