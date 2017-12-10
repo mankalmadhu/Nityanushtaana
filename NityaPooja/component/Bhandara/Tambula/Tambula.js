fd169b37e4249044fd610910fe474bbd451c33d9
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-10 21:47:18 +0100

    Tambula

---
+++ NityaPooja/component/Bhandara/Tambula/Tambula.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
+import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
+import ಆವರಣ from '../../Avarana/Avarana';
+import ವಿಧಾನ from '../../Vishaya/Vidhana';
+ 
+export default ತಾ೦ಬೂಲ = () => (
+    <ಆವರಣ>
+    <ಶೀರ್ಷಿಕೆ>
+      ತಾ೦ಬೂಲ
+    </ಶೀರ್ಷಿಕೆ>
+    <ವಿನಿಯೋಗ >
+ 
+     ನಾಭ್ಯಾ॑ ಆಸೀದ॒ಂತರಿ॑ಕ್ಷಮ್ । ಶೀ॒ರ್ಷ್ಣೋ  ದ್ಯೌಸ್ಸಮ॑ವರ್ತತ । ಪ॒ದ್ಭ್ಯಾಂ ಭೂಮಿ॒ರ್ದಿಶ॒ಶ್ಯೋತ್ರಾ᳚ತ್ । ತಥಾ॑ ಲೋ॒ಕಾꣳ ಅ॑ಕಲ್ಪಯನ್ ।
+
+     </ವಿನಿಯೋಗ>
+    <ಪೂರ್ವಾ೦ಗ>
+
+      ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಪೂಗಿಫಲ ತಾ೦ಬೂಲ೦ ನಿವೇದಯಾಮಿ ॥
+      
+    </ಪೂರ್ವಾ೦ಗ>
+
+    </ಆವರಣ>
+);
---
+++ NityaPooja/component/Bhandara/Tambula/__tests__/Tambula.js
@@ -0,0 +1,19 @@
+import 'react-native';
+import React from 'react';
+import ತಾ೦ಬೂಲ from '../Tambula';
+
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../../Avarana/Avarana',()=>'ಆವರಣ');
+jest.mock('../../../Vishaya/Sheershike',()=>'ಶೀರ್ಷಿಕೆ');
+jest.mock('../../../Vishaya/Purvanga',()=>'ಪೂರ್ವಾ೦ಗ');
+jest.mock('../../../Vishaya/Viniyoga',()=>'ವಿನಿಯೋಗ');
+jest.mock('../../../Vishaya/Vidhana',()=>'ವಿಧಾನ');
+
+test("ತಾ೦ಬೂಲ" , () => {
+
+expect(Renderer.create(
+    <ತಾ೦ಬೂಲ />
+).toJSON()).toMatchSnapshot();
+
+});
