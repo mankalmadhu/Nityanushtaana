535316b9543b691b05bfc7911f685b675e12e7f2
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-10 20:43:59 +0100

    Deepa

---
+++ NityaPooja/component/Bhandara/Deepa/Deepa.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
+import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
+import ಆವರಣ from '../../Avarana/Avarana';
+import ವಿಧಾನ from '../../Vishaya/Vidhana';
+ 
+export default ದೀಪ = () => (
+    <ಆವರಣ>
+    <ಶೀರ್ಷಿಕೆ>
+      ದೀಪ
+    </ಶೀರ್ಷಿಕೆ>
+    <ವಿನಿಯೋಗ >
+ 
+    ಬ್ರಾ॒ಹ್ಮ॒ಣೋ᳚ಽಸ್ಯ॒ ಮುಖ॑ಮಾಸೀತ್ । ಬಾ॒ಹೂ ರಾ॑ಜ॒ನ್ಯಃ॑ ಕೃ॒ತಃ । ಊ॒ರೂ ತದ॑ಸ್ಯ॒ ಯದ್ವೈಶ್ಯಃ॑ । ಪ॒ದ್ಭ್ಯಾꣳ ಶೂ॒ದ್ರೋ ಅ॑ಜಾಯತ ।
+
+     </ವಿನಿಯೋಗ>
+    <ಪೂರ್ವಾ೦ಗ>
+      ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ದೀಪ೦ ದರ್ಶಯಾಮಿ ॥
+    </ಪೂರ್ವಾ೦ಗ>
+
+    </ಆವರಣ>
+);
