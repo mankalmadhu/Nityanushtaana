fd5cf2528d045a533abd8a867922e9312325dc95
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-16 21:12:34 +0100

    Namaskara

---
+++ NityaPooja/component/Bhandara/Namaskara/__tests__/Namaskara.js
@@ -0,0 +1,19 @@
+import 'react-native';
+import React from 'react';
+import ನಮಸ್ಕಾರ from '../Namaskara';
+ 
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../../Avarana/Avarana',()=>'ಆವರಣ');
+jest.mock('../../../Vishaya/Sheershike',()=>'ಶೀರ್ಷಿಕೆ');
+jest.mock('../../../Vishaya/Purvanga',()=>'ಪೂರ್ವಾ೦ಗ');
+jest.mock('../../../Vishaya/Viniyoga',()=>'ವಿನಿಯೋಗ');
+jest.mock('../../../Vishaya/Vidhana',()=>'ವಿಧಾನ');
+
+test("ನಮಸ್ಕಾರ" , () => {
+
+expect(Renderer.create(
+    <ನಮಸ್ಕಾರ />
+).toJSON()).toMatchSnapshot();
+
+});
