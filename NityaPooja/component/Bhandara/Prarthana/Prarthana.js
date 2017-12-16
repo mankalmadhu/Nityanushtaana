f2c8c6b5e9b3f912272a10f33397be02382c9f7a
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-16 21:46:25 +0100

    Prarthana

---
+++ NityaPooja/component/Bhandara/Prarthana/Prarthana.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
+import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
+import ಆವರಣ from '../../Avarana/Avarana';
+import ವಿಧಾನ from '../../Vishaya/Vidhana';
+
+export default ಪ್ರಾರ್ಥನ = () => (
+    <ಆವರಣ>
+    <ಶೀರ್ಷಿಕೆ>
+       ಪ್ರಾರ್ಥನ
+    </ಶೀರ್ಷಿಕೆ>
+    <ವಿನಿಯೋಗ >
+    
+    ಋಣರೋಗಾದಿ ದಾರಿದ್ರ್ಯ ಪಾಪಕ್ಷುದಪಮೃತ್ಯವಃ ।
+    ಭಯಶೋಕಮನಸ್ತಾಪಾಃ ನಶ್ಯ೦ತು ಮಮ ಸರ್ವದಾ ॥
+
+     </ವಿನಿಯೋಗ>
+    <ಪೂರ್ವಾ೦ಗ>
+
+      ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಮನೋರಥಾನ್ ಪ್ರಾರ್ಥಯಾಮಿ ॥
+      
+    </ಪೂರ್ವಾ೦ಗ>
+
+    </ಆವರಣ>
+);
---
+++ NityaPooja/component/Bhandara/Prarthana/__tests__/Prarthana.js
@@ -0,0 +1,19 @@
+import 'react-native';
+import React from 'react';
+import ಪ್ರಾರ್ಥನ from '../Prarthana';
+
+import Renderer from 'react-test-renderer';
+
+jest.mock('../../../Avarana/Avarana',()=>'ಆವರಣ');
+jest.mock('../../../Vishaya/Sheershike',()=>'ಶೀರ್ಷಿಕೆ');
+jest.mock('../../../Vishaya/Purvanga',()=>'ಪೂರ್ವಾ೦ಗ');
+jest.mock('../../../Vishaya/Viniyoga',()=>'ವಿನಿಯೋಗ');
+jest.mock('../../../Vishaya/Vidhana',()=>'ವಿಧಾನ');
+
+test("ಪ್ರಾರ್ಥನ" , () => {
+
+expect(Renderer.create(
+    <ಪ್ರಾರ್ಥನ />
+).toJSON()).toMatchSnapshot();
+
+});
