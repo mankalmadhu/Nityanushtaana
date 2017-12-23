e392c1217b14d2c7c5dcb45353356f87cea71c37
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-23 19:49:20 +0100

    Padya

--- NityaPooja/component/Bhandara/Padya/Padya.js
+++ NityaPooja/component/Bhandara/Padya/Padya.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
-import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಪೂರ್ವೋತ್ತರ from '../../Vishaya/Purvanga';
 import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
 import ಆವರಣ from '../../Avarana/Avarana';
 import ವಿಧಾನ from '../../Vishaya/Vidhana';
@@ -16,9 +16,13 @@ export default ಪಾದ್ಯ = () => (
     ಏ॒ತಾವಾ॑ನಸ್ಯ ಮಹಿ॒ಮಾ । ಅತೋ॒ ಜ್ಯಾಯಾಗ್॑ಶ್ಚ॒ ಪೂರು॑ಷಃ । ಪಾದೋ᳚ಽಸ್ಯ॒ ವಿಶ್ವಾ॑ ಭೂ॒ತಾನಿ॑ । ತ್ರಿ॒ಪಾದ॑ಸ್ಯಾ॒ಮೃತಂ॑ ದಿ॒ವಿ ।
 
      </ವಿನಿಯೋಗ>
-    <ಪೂರ್ವಾ೦ಗ>
+    <ಪೂರ್ವೋತ್ತರ>
       ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಪಾದ್ಯ೦ ಪಾದ್ಯ೦ ಸಮರ್ಪಯಾಮಿ ॥
-    </ಪೂರ್ವಾ೦ಗ>
+    </ಪೂರ್ವೋತ್ತರ>
+
+    <ವಿಧಾನ>
+      (ಎರಡು ಉದ್ಧರಣೆ ಕಲಶೋದಕವನ್ನು ವಿಸರ್ಜಿಸುವುದು.)
+    </ವಿಧಾನ>
 
     </ಆವರಣ>
 );
