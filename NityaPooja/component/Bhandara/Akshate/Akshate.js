5fa2968bfc4bec70747ae946172a5d2e7f6d88f2
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-23 20:31:10 +0100

    Akshate

--- NityaPooja/component/Bhandara/Akshate/Akshate.js
+++ NityaPooja/component/Bhandara/Akshate/Akshate.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
-import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಪೂರ್ವೋತ್ತರ from '../../Vishaya/Purvanga';
 import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
 import ಆವರಣ from '../../Avarana/Avarana';
 import ವಿಧಾನ from '../../Vishaya/Vidhana';
- 
+
 export default ಅಕ್ಷತೆ = () => (
     <ಆವರಣ>
     <ಶೀರ್ಷಿಕೆ>
@@ -16,9 +16,9 @@ export default ಅಕ್ಷತೆ = () => (
     ತಸ್ಮಾ᳚ದ್ಯ॒ಜ್ಞಾಥ್ಸ॑ರ್ವ॒ಹುತಃ॑ । ಋಚ॒ಸ್ಸಾಮಾ॑ನಿ ಜಜ್ಞಿರೇ । ಛ೦ದಾಗ್॑೦ಸಿ ಜಜ್ಞಿರೇ॒ ತಸ್ಮಾ᳚ತ್ । ಯಜು॒ಸ್ತಸ್ಮಾ॑ದಜಾಯತ ।
 
      </ವಿನಿಯೋಗ>
-    <ಪೂರ್ವಾ೦ಗ>
+    <ಪೂರ್ವೋತ್ತರ>
       ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಅಕ್ಷತಾನ್ ಸಮರ್ಪಯಾಮಿ ॥
-    </ಪೂರ್ವಾ೦ಗ>
+    </ಪೂರ್ವೋತ್ತರ>
 
     </ಆವರಣ>
 );
