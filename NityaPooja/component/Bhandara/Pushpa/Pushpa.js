82306ab6b02cfad646492e287c04eab268af8613
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-23 20:32:59 +0100

    Pushpa

--- NityaPooja/component/Bhandara/Pushpa/Pushpa.js
+++ NityaPooja/component/Bhandara/Pushpa/Pushpa.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
-import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಪೂರ್ವೋತ್ತರ from '../../Vishaya/Purvanga';
 import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
 import ಆವರಣ from '../../Avarana/Avarana';
 import ವಿಧಾನ from '../../Vishaya/Vidhana';
- 
+
 export default ಪುಷ್ಪ = () => (
     <ಆವರಣ>
     <ಶೀರ್ಷಿಕೆ>
@@ -16,9 +16,9 @@ export default ಪುಷ್ಪ = () => (
     ತಸ್ಮಾ॒ದಶ್ವಾ॑ ಅಜಾಯಂತ । ಯೇ ಕೇ ಚೋ॑ಭ॒ಯಾದ॑ತಃ । ಗಾವೋ॑ ಹ ಜಜ್ಞಿರೇ॒ ತಸ್ಮಾ᳚ತ್ । ತಸ್ಮಾ᳚ಜ್ಜಾ॒ತಾ ಅ॑ಜಾ॒ವಯಃ॑ ।
 
      </ವಿನಿಯೋಗ>
-    <ಪೂರ್ವಾ೦ಗ>
+    <ಪೂರ್ವೋತ್ತರ>
       ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಪುಷ್ಪಾಣಿ/ಪುಷ್ಪಮಾಲಿಕಾ೦ ಸಮರ್ಪಯಾಮಿ ॥
-    </ಪೂರ್ವಾ೦ಗ>
+    </ಪೂರ್ವೋತ್ತರ>
 
     </ಆವರಣ>
 );
