e50263492be6303ba8eb59f3657ba17ae0c6d22f
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-23 20:38:49 +0100

    Dhupa

--- NityaPooja/component/Bhandara/Dhupa/Dhupa.js
+++ NityaPooja/component/Bhandara/Dhupa/Dhupa.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
-import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಪೂರ್ವೋತ್ತರ from '../../Vishaya/Purvanga';
 import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
 import ಆವರಣ from '../../Avarana/Avarana';
 import ವಿಧಾನ from '../../Vishaya/Vidhana';
- 
+
 export default ಧೂಪ = () => (
     <ಆವರಣ>
     <ಶೀರ್ಷಿಕೆ>
@@ -16,9 +16,12 @@ export default ಧೂಪ = () => (
     ಯತ್ಪುರು॑ಷಂ॒ ವ್ಯ॑ದಧುಃ । ಕ॒ತಿ॒ಧಾ ವ್ಯ॑ಕಲ್ಪಯನ್ । ಮುಖಂ॒ ಕಿಮ॑ಸ್ಯ॒ ಕೌ ಬಾ॒ಹೂ । ಕಾವೂ॒ರೂ ಪಾದಾ॑ವುಚ್ಯೇತೇ ।
 
      </ವಿನಿಯೋಗ>
-    <ಪೂರ್ವಾ೦ಗ>
+    <ಪೂರ್ವೋತ್ತರ>
       ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಧೂಪಮಾಘ್ರಾಪಯಾಮಿ ॥
-    </ಪೂರ್ವಾ೦ಗ>
+    </ಪೂರ್ವೋತ್ತರ>
+    <ವಿಧಾನ>
+     (ಗ೦ಧದ ಕಡ್ಡಿ/ದಶಾ೦ಗವನ್ನು ದೇವರಿಗೆ ಆಘ್ರಾಣಿಸುವುದು.)
+    </ವಿಧಾನ>
 
     </ಆವರಣ>
 );
