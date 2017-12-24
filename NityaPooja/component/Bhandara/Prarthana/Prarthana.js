b3813768c6a2ccc284bd34543301f654233b6700
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-24 21:07:00 +0100

    Prarthana

--- NityaPooja/component/Bhandara/Prarthana/Prarthana.js
+++ NityaPooja/component/Bhandara/Prarthana/Prarthana.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
-import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಪೂರ್ವೋತ್ತರ from '../../Vishaya/Purvanga';
 import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
 import ಆವರಣ from '../../Avarana/Avarana';
 import ವಿಧಾನ from '../../Vishaya/Vidhana';
@@ -17,11 +17,11 @@ export default ಪ್ರಾರ್ಥನ = () => (
     ಭಯಶೋಕಮನಸ್ತಾಪಾಃ ನಶ್ಯ೦ತು ಮಮ ಸರ್ವದಾ ॥
 
      </ವಿನಿಯೋಗ>
-    <ಪೂರ್ವಾ೦ಗ>
+    <ಪೂರ್ವೋತ್ತರ>
 
       ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ । ಮನೋರಥಾನ್ ಪ್ರಾರ್ಥಯಾಮಿ ॥
       
-    </ಪೂರ್ವಾ೦ಗ>
+    </ಪೂರ್ವೋತ್ತರ>
 
     </ಆವರಣ>
 );
