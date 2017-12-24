df58150d73cf034b694ee13d083f31e801b28a7e
Madhu Mankal <mankalmadhu@gmail.com>
2017-12-24 21:05:31 +0100

    Punararghya

--- NityaPooja/component/Bhandara/Punararghya/Punararghya.js
+++ NityaPooja/component/Bhandara/Punararghya/Punararghya.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ವಿನಿಯೋಗ from '../../Vishaya/Viniyoga';
-import ಪೂರ್ವಾ೦ಗ from '../../Vishaya/Purvanga';
+import ಪೂರ್ವೋತ್ತರ from '../../Vishaya/Purvanga';
 import ಶೀರ್ಷಿಕೆ from '../../Vishaya/Sheershike';
 import ಆವರಣ from '../../Avarana/Avarana';
 import ವಿಧಾನ from '../../Vishaya/Vidhana';
@@ -16,11 +16,14 @@ export default ಪುನರ್ರಘ್ಯ = () => (
      ಓ೦ ತತ್ಸ॑ವಿ॒ತುರ್ವರೇ᳚ಣ್ಯ॒೦ ಭರ್ಗೋ᳚ ದೇ॒ವಸ್ಯ॑ ಧೀಮಹಿ । ಧಿಯೋ॒ ಯೋ ನಃ॑ ಪ್ರಚೋ॒ದಯಾ᳚ತ್ ॥
 
      </ವಿನಿಯೋಗ>
-    <ಪೂರ್ವಾ೦ಗ>
+    <ಪೂರ್ವೋತ್ತರ>
 
       ಶ್ರೀ ಇಷ್ಟ ದೇವತಾಭ್ಯೋ ನಮಃ ।  ಪ್ರಸನ್ನಾರ್ಘ್ಯ೦ ಸಮರ್ಪಯಾಮಿ ॥
       
-    </ಪೂರ್ವಾ೦ಗ>
+    </ಪೂರ್ವೋತ್ತರ>
+    <ವಿಧಾನ>
+    (ಎರಡು ಉದ್ಧರಣೆ ಕಲಶೋದಕವನ್ನು ವಿಸರ್ಜಿಸುವುದು.)
+   </ವಿಧಾನ>
 
     </ಆವರಣ>
 );
