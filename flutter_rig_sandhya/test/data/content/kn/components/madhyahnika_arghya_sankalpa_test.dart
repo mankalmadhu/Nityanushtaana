d8bac46c353db060685dbb3861dbd3141e767e1f
Madhu Ahobalan <mankalmadhu@gmail.com>
2026-08-14 23:54:52 +0530

    fix: update tests for madhyahnika sankalpa changes

--- flutter_rig_sandhya/test/data/content/kn/components/madhyahnika_arghya_sankalpa_test.dart
+++ flutter_rig_sandhya/test/data/content/kn/components/madhyahnika_arghya_sankalpa_test.dart
@@ -14,13 +14,33 @@ void main() {
         final component = Component.fromJson(
           jsonDecode(file.readAsStringSync()),
         );
-        expect(component.blocks.length, 2);
+        expect(component.blocks.length, 6);
         expect(component.blocks[0].type, 'heading');
         expect(component.blocks[0].text, 'ಅರ್ಘ್ಯ ಸ೦ಕಲ್ಪ');
         expect(component.blocks[1].type, 'verse_sankalpa');
         expect(
           component.blocks[1].text,
-          'ಆಚಮ್ಯ, ಪ್ರಾಣಾನಾಯಮ್ಯ, ದೇಶಕಾಲೌ ಸ೦ಕೀರ್ತ್ಯ, ಏವ೦ಗುಣ ವಿಶೇಷಣ ವಿಶಿಷ್ಟಾಯಾ೦ ಶುಭತಿಥೌ\nಮಮ ಉಪಾತ್ತ ಸಮಸ್ತ ದುರಿತಕ್ಷಯದ್ವಾರಾ ಶ್ರೀ ಪರಮೇಶ್ವರ ಪ್ರೀತ್ಯರ್ಥ೦ ಮಾಧ್ಯಾಹ್ನಿಕ ಸ೦ಧ್ಯಾ೦ಗ ಅರ್ಘ್ಯ ಪ್ರಧಾನಾನಿ ಕರಿಷ್ಯೇ ॥',
+          'ಪ್ರಣವಸ್ಯ ಪರಬ್ರಹ್ಮ ಋಷಿಃ ಪರಮಾತ್ಮಾ ದೇವತಾ ದೈವೀ ಗಾಯತ್ರೀ ಛ೦ದಃ ಪ್ರಾಣಾಯಾಮೇ ವಿನಿಯೋಗಃ ।',
+        );
+        expect(component.blocks[2].type, 'verse_viniyoga');
+        expect(
+          component.blocks[2].text,
+          'ಓ೦ ಭೂಃ ಓ೦ ಭುವಃ ಓ೦ ಸುವಃ ಓ೦ ಮಹಃ ಓ೦ ಜನಃ ಓ೦ ತಪಃ ಓ೦ ಸತ್ಯಮ್ ॥',
+        );
+        expect(component.blocks[3].type, 'verse_viniyoga');
+        expect(
+          component.blocks[3].text,
+          'ಓ೦ ತತ್ಸ॑ವಿ॒ತುರ್ವರೇ᳚ಣ್ಯ॒೦ ಭರ್ಗೋ᳚ ದೇ॒ವಸ್ಯ॑ ಧೀಮಹಿ । ಧಿಯೋ॒ ಯೋ ನಃ॑ ಪ್ರಚೋ॒ದಯಾ᳚ತ್ ॥',
+        );
+        expect(component.blocks[4].type, 'verse_viniyoga');
+        expect(
+          component.blocks[4].text,
+          'ಓ॒ಮಾಪೋ॒ ಜ್ಯೋತೀ॒ರಸೋ॒ಽಮೃತ॒೦ ಬ್ರಹ್ಮ॒ ಭೂರ್ಭುವ॒ಸ್ಸ್ವರೋ೦॥',
+        );
+        expect(component.blocks[5].type, 'verse_sankalpa');
+        expect(
+          component.blocks[5].text,
+          'ಮಮ ಉಪಾತ್ತ ಸಮಸ್ತ ದುರಿತಕ್ಷಯದ್ವಾರಾ ಶ್ರೀ ಪರಮೇಶ್ವರ ಪ್ರೀತ್ಯರ್ಥ೦ ಮಾಧ್ಯಾಹ್ನಿಕ ಸ೦ಧ್ಯಾ೦ಗ ಅರ್ಘ್ಯ ಪ್ರದಾನಾನಿ ಕರಿಷ್ಯೇ ॥',
         );
       },
     );
